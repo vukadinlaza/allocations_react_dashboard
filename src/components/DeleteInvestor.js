a9d6ce34dbc10b352e01dd4519e98354b11729f7
Tanver Hasan <t.h.noman@outlook.com>
2019-11-01 21:50:43 +0000

    Created link for agreements

--- src/components/DeleteInvestor.js
+++ src/components/DeleteInvestor.js
@@ -4,14 +4,13 @@ import { gql } from 'apollo-boost';
 import { Button } from 'reactstrap';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import React from "react"
-const MUTATION_QUERY=gql`
-mutation deleteInvestorById($id:ID!){
-  deleteInvestorById(id:$id)
-}
-`;
+import { deleteInvestorById } from '../api/query';
+
+
+
 const DeleteInvestor=(props)=>{
     const id=props.id;
- const [deleteInvestorById, { data }, error, loading] = useMutation(MUTATION_QUERY);
+ const [DELETE_INVESTOR_BY_ID, { data }, error, loading] = useMutation(deleteInvestorById);
 
  if (loading) return <p>Loading...</p>;
  if (error) return <p>Error :(</p>;
@@ -19,7 +18,7 @@ const DeleteInvestor=(props)=>{
 const handleDeleteButtonClick=()=>{
     console.log("deleted");
     console.log(id);
-    deleteInvestorById({ variables: { id: id } });
+    DELETE_INVESTOR_BY_ID({ variables: { id: id } });
 }
 
  return(
