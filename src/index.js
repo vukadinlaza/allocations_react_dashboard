e71e835bccc0889e34b05b233c9ead1475d8c4e9
Kingsley <kadvani1@gmail.com>
2019-11-04 12:49:47 -0800

    Revert "Switched to custom domain"
    This reverts commit 19e058be5df3bd3ce2b29211ed752dc0ccad200f.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
+import config from "./auth_config.json";
 import history from "./utils/history";
 
-const domain="login.allocations.co";
+const domain="allocations1.auth0.com";
 const clientId="R2iJsfjNPGNjIdPmRoE3IcKd9UvVrsp1";
 const audience="https://api.graphql.com"
 
