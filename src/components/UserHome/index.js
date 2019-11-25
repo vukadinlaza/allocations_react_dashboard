10a0e388e4c37482e63259f828dcb2267b9968c2
Will Sheehan <willsheehan95@gmail.com>
2019-11-25 15:17:45 -0500

    use new API structure in UserHome

--- src/components/UserHome/index.js
+++ src/components/UserHome/index.js
@@ -1,22 +1,14 @@
 import React, { useEffect } from 'react'
+import _ from 'lodash'
 import { gql } from 'apollo-boost'
 import { Link } from 'react-router-dom'
-import { useQuery } from '@apollo/react-hooks';
+import { useLazyQuery } from '@apollo/react-hooks';
 import { useAuth0 } from "../../react-auth0-spa";
-import { Row, Container } from 'reactstrap'
+import { Row, Container, Col } from 'reactstrap'
 import { Button } from '@material-ui/core' 
 import { nWithCommas} from '../../utils/numbers'
 import Chart from "react-google-charts"
 import "./style.scss";
-
-const data = {
-  first_name: "Nils",
-  last_name: "De Jonge",
-  deals: [
-    { company_name: "OrbitFab", amount: 5000 },
-    { company_name: "Oncosenx", amount: 5000 }
-  ]
-}
 
 const purples = [
   "#6200EE",
@@ -29,12 +21,14 @@ const chartOptions = {
   headerHeight: 0,
   fontColor: "#fff",
   highlightColor: "#fff",
-  showTooltips: false
+  showTooltips: false,
+  maxDepth: 1,
+  maxPostDepth: 2
   // showScale: true
 };
 
-function formatData(deals) {
-  const d = deals.map((d, i) => [d.company_name, 'All', d.amount, d.amount - (i * 5000)])
+function formatData(investments) {
+  const d = investments.map((d, i) => [d.deal.company_name, 'All', d.amount, d.amount - (i * 5000)])
 
   return [
     ['Company', 'Group', 'Amount Invested (size)', 'Company Color (color)'],
@@ -42,19 +36,39 @@ function formatData(deals) {
   ].concat(d)
 }
 
+const GET_INVESTOR = gql`
+  query GetInvestor($email: String!) {
+    investor(email: $email) {
+      _id
+      first_name
+      last_name
+      email
+      investments {
+        amount
+        deal {
+          company_name
+          company_description
+          date_closed
+        }
+      }
+    }
+  }
+`
+
 export default function UserHome () {
   const { user } = useAuth0()
+  const [getInvestor, { data, error }] = useLazyQuery(GET_INVESTOR)
 
   useEffect(() => {
-    if (data) {
-      // renderChart()
+    if (user && user.email) {
+      getInvestor({ variables: { email: user.email }})
     }
-  }, [data])
+  }, [user])
 
   if (!data) return <div>Loading...</div>
+  const investor = data.investor
 
-  const investor = data
-  const total_invested = 10000
+  const total_invested = _.sumBy(investor.investments, 'amount') || 0
 
   if (investor) {
     investor.invited_deals = []
@@ -62,7 +76,7 @@ export default function UserHome () {
   return (
     <Container fluid className="UserHome">
       <Row>
-        <div className="welcome col-3 offset-2">
+        <Col md={{size: 3, offset: 2}} sm={{size: 5, offset: 0}} className="welcome">
           <div className="tile tile-top">
             <div className="welcome-text">Welcome,<br></br>{investor.first_name}</div>
             <div className="welcome-desc">
@@ -74,8 +88,8 @@ export default function UserHome () {
               </div>
             </div>
           </div>
-        </div>
-        <div className="total-investments col-5">
+        </Col>
+        <Col md="5" sm="7" className="total-investments">
           <div className="tile tile-top">
             <div className="small-header">Total Investments</div>
             <div className="amount">${nWithCommas(total_invested)}</div>
@@ -83,37 +97,47 @@ export default function UserHome () {
               <Chart chartType="TreeMap"
                 width="100%"
                 height="125px"
-                data={formatData(data.deals)}
+                data={formatData(investor.investments)}
                 options={chartOptions} />
             </div>
           </div>
-        </div>
+        </Col>
       </Row>
       <Row>
-        <div className="last-deals col-4 offset-2">
+        <Col md={{size: 4, offset: 2}} sm={{size: 6, offset: 0}} className="last-deals">
           <div className="tile tile-bottom">
             <div className="small-header">Most Recent Deals</div>
-            {investor.deals.map((deal, i) => (
-              <DealStub key={i} deal={deal} />
+            {investor.investments.map((investment, i) => (
+              <InvestmentStub key={i} investment={investment} />
             ))}
           </div>
-        </div>
-        <div className="last-deal col-4">
+        </Col>
+        <Col md="4" sm="6" className="last-deal">
           <div className="tile tile-bottom">
             <div className="small-header">Invited Deals</div>
             {investor.invited_deals.map((deal, i) => (
               <DealStub key={i} deal={deal} />
             ))}
           </div>
-        </div>
+        </Col>
       </Row>
     </Container>
   )
 }
 
+function InvestmentStub ({ investment }) {
+  return (
+    <div key={investment._id} className="investment-stub">
+      <span>{investment.deal.company_name}</span>
+      <span>${nWithCommas(investment.amount)}</span>
+      <span>{investment.deal.date_closed}</span>
+    </div>
+  )
+}
+
 function DealStub ({ deal }) {
   return (
-    <div key={deal.id} className="deal-stub">
+    <div key={deal._id} className="investment-stub">
       <span>{deal.company_name}</span>
       <span>{nWithCommas(deal.amount)}</span>
       <span>{deal.date_closed}</span>
