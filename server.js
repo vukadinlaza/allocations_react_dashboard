f20684c84c2870330aad9ce447ec8577d417e49e
Will Sheehan <willsheehan95@gmail.com>
2019-12-03 10:03:00 -0500

    force-ssl in prod

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 const express = require("express");
 const { join } = require("path");
 const morgan = require("morgan");
+const sslRedirect = require("heroku-ssl-redirect");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.static(join(__dirname, "build")));
+app.use(sslRedirect());
 
 app.use((_, res) => {
   res.sendFile(join(__dirname, "build", "index.html"));
