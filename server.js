fedd37f73bccbaa34588160223b79a5bd62a8b7e
Will Sheehan <willsheehan95@gmail.com>
2019-12-05 13:04:50 -0500

    use heroku ssl-redirect

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ app.use(express.static(join(__dirname, "build")));
 
 console.log("Starting Server: ", process.env.NODE_ENV)
 
-if (process.env.NODE_ENV === "production") {
-  console.log("In production - will redirect to https")
-  app.get("*", function(request, response) {
-    response.redirect("https://" + request.headers.host + request.url);
-  })
-}
+// if (process.env.NODE_ENV === "production") {
+//   console.log("In production - will redirect to https")
+//   app.get("*", function(request, response) {
+//     response.redirect("https://" + request.headers.host + request.url);
+//   })
+// }
+
+app.use(sslRedirect())
 
 app.use((_, res) => {
   res.sendFile(join(__dirname, "build", "index.html"));
