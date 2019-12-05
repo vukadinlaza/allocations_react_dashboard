61638763aa5ce1b73f91a47294702900cca187cd
Will Sheehan <willsheehan95@gmail.com>
2019-12-05 13:25:02 -0500

    new redirect scheme

--- server.js
+++ server.js
@@ -12,14 +12,18 @@ app.use(express.static(join(__dirname, "build")));
 
 console.log("Starting Server: ", process.env.NODE_ENV)
 
-// if (process.env.NODE_ENV === "production") {
-//   console.log("In production - will redirect to https")
-//   app.get("*", function(request, response) {
-//     response.redirect("https://" + request.headers.host + request.url);
-//   })
-// }
-
-app.use(sslRedirect())
+if (process.env.NODE_ENV === "production") {
+  console.log("In production - will redirect to https")
+  app.use (function (req, res, next) {
+    if (req.secure) {
+      // request was via https, so do no special handling
+      next();
+    } else {
+      // request was via http, so redirect to https
+      res.redirect('https://' + req.headers.host + req.url);
+    }
+  });
+}
 
 app.use((_, res) => {
   res.sendFile(join(__dirname, "build", "index.html"));
