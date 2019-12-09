5688785f1eaa6f426bce5ed6956d82ade23f6515
Will Sheehan <willsheehan95@gmail.com>
2019-12-09 14:52:11 -0500

    signUp now invites you to a deal if 'key' exists

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { gql } from "apollo-boost"
-import { Redirect } from "react-router-dom"
+import { Redirect, useLocation } from "react-router-dom"
 import { useMutation } from '@apollo/react-hooks'
 import { useAuth0 } from "../react-auth0-spa"
+import queryString from 'query-string'
 
 const SIGNUP = gql`
-  mutation SignUp {
-    signUp {
+  mutation SignUp($inviteKey: String) {
+    signUp(inviteKey: $inviteKey) {
       _id
       email
       first_name
@@ -21,15 +22,18 @@ function dataComplete (user) {
 }
 
 export default function SignUp () {
+  const { search } = useLocation()
   const [signedIn, setSignedIn] = useState(false)
   const { user, isAuthenticated, loginWithRedirect } = useAuth0()
   const [signUp, {data, loading, error, called}] = useMutation(SIGNUP)
+
+  const { key } = queryString.parse(search)
 
   useEffect(() => {
     const fn = async () => {
       if (!isAuthenticated) {
         await loginWithRedirect({
-          appState: { targetUrl: "/signup" },
+          appState: { targetUrl: `/signup${search}` },
           initialScreen: 'signUp',
         });
       }
@@ -38,7 +42,7 @@ export default function SignUp () {
   }, [isAuthenticated, loginWithRedirect]);
 
   useEffect(() => {
-    if (isAuthenticated && !called) signUp()
+    if (isAuthenticated && !called) signUp({ variables: { inviteKey: key }})
   }, [isAuthenticated])
 
   useEffect(() => {
