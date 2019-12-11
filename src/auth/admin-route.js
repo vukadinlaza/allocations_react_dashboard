28b41c8732dcbb314f16a104c4f49d57745635e8
Will Sheehan <willsheehan95@gmail.com>
2019-12-11 15:52:58 -0500

    add hans to admin whitelist

--- src/auth/admin-route.js
+++ src/auth/admin-route.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom"
 import { useAuth0 } from "../react-auth0-spa";
 
-export const adminWhitelist = ["will.sheehan@toptal.com", "kadvani1@gmail.com", "michelle@allocations.co"]
+export const adminWhitelist = ["will.sheehan@toptal.com", "kadvani1@gmail.com", "michelle@allocations.co", "hanspaulpizzinini@gmail.com"]
 
 export default function AdminRoute ({ children, ...rest }) {
   const { isAuthenticated, loginWithRedirect, user } = useAuth0();
