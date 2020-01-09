7f63dc5ab343e104b1b1f06509a12a05a7569cb3
Will Sheehan <willsheehan95@gmail.com>
2020-01-09 14:10:42 -0500

    fix admin route to use new Auth flow

--- src/auth/admin-route.js
+++ src/auth/admin-route.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom"
+import { Route, Redirect, useHistory } from "react-router-dom"
 import { useAuth0 } from "../react-auth0-spa";
+import Loader from '../components/utils/Loader'
 
 export const adminWhitelist = ["will.sheehan@toptal.com", "kadvani1@gmail.com", "michelle@allocations.co", "hanspaulpizzinini@gmail.com"]
 
 export default function AdminRoute ({ children, ...rest }) {
+  const history = useHistory()
   const { isAuthenticated, loginWithRedirect, user } = useAuth0();
 
-  return (
-    <Route {...rest}
-      render={({ location }) => 
-        isAuthenticated && user && adminWhitelist.includes(user.email) 
-          ? (children)
-          : <Redirect to="/" />
-      }
-    />
-  )
+  if (isAuthenticated === null || !user) return <Loader />
+
+  if (isAuthenticated === true && adminWhitelist.includes(user.email)) {
+    return <Route {...rest}>{children}</Route>
+  } else {
+    return <Redirect to="/" />
+  }
 }
