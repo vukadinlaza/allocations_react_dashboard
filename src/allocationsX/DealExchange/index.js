e89132ff0d00af358359cf9cce854b5376c71701
Will Sheehan <willsheehan95@gmail.com>
2020-02-27 13:41:41 -0500

    Some improvements for AllocationsX

--- src/allocationsX/DealExchange/index.js
+++ src/allocationsX/DealExchange/index.js
@@ -29,41 +29,43 @@ export default function ExchangeDeal () {
   const { data, error, loading } = useQuery(DEAL, { variables: { slug: deal } })
 
   if (!data) return <Loader />
-
-  console.log({data})
   const { exchangeDeal } = data
 
   return (
     <div className="AllocationsX-home">
       <div className="AllocationsX-header">
-        <h4>{exchangeDeal.company_name}</h4>
-        <p>{exchangeDeal.company_description}</p>
-        <div className="info">
-          <span className="header-card">
-            <div>Your SPV shares</div>
-            <div>10,000</div>
-          </span>
-          <span className="header-card">
-            <div>Nominal Value of Your Holding</div>
-            <div>$10,000</div>
-          </span>
-          {/**<span className="header-card">
-            <div>Estimated Current Value of your Holding</div>
-            <div>$65,000</div>
-          </span>
-          <span className="header-card">
-            <div>Estimated Share Value</div>
-            <div>$6.50</div>
-          </span>**/}
-          <span className="header-card">
-            <div>Initial Share Price</div>
-            <div>$1</div>
-          </span>
-          <span className="header-card">
-            <div>Last Trade Price</div>
-            <div>-</div>
-          </span>
-        </div>
+        <Row>
+          <Col md={{size: 10, offset: 1}}>
+            <h4>{exchangeDeal.company_name}</h4>
+            <p>{exchangeDeal.company_description}</p>
+            <div className="info">
+              <span className="header-card">
+                <div>Your SPV shares</div>
+                <div>10,000</div>
+              </span>
+              <span className="header-card">
+                <div>Nominal Value of Your Holding</div>
+                <div>$10,000</div>
+              </span>
+              {/**<span className="header-card">
+                <div>Estimated Current Value of your Holding</div>
+                <div>$65,000</div>
+              </span>
+              <span className="header-card">
+                <div>Estimated Share Value</div>
+                <div>$6.50</div>
+              </span>**/}
+              <span className="header-card">
+                <div>Initial Share Price</div>
+                <div>$1</div>
+              </span>
+              <span className="header-card">
+                <div>Last Trade Price</div>
+                <div>-</div>
+              </span>
+            </div>
+          </Col>
+        </Row>
       </div>
       <Row>
         <Col sm="5" md={{size: 4, offset: 1}}>
@@ -85,37 +87,6 @@ function Stats () {
   )
 }
 
-function OrderForm () {
-  const [order, setOrder] = useSimpleReducer({price: "", amount: "", direction: "buy"})
-
-  return (
-    <div className="OrderForm">
-      <FormControl style={{width: "25%", marginRight: "5%"}}>
-        <InputLabel>Side</InputLabel>
-        <Select value={order.direction}
-          onChange={e => setOrder({ direction: e.target.value })}
-          inputProps={{name: 'Type'}}>
-          <MenuItem value="sell">SELL</MenuItem>
-          <MenuItem value="buy">BUY</MenuItem>
-        </Select>
-      </FormControl>
-      <TextField value={order.price}
-        style={{width: "70%", marginBottom: "10px"}}
-        onChange={e => setOrder({ price: e.target.value })}
-        label="Shares"
-      />
-      <TextField value={order.price}
-        style={{width: "100%", marginBottom: "10px"}}
-        onChange={e => setOrder({ price: e.target.value })}
-        label="Price"
-        InputProps={{
-          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-        }}
-      />
-    </div>
-  )
-}
-
 const book = {
   bids: [
     { price: 4.05, amount: 2500 },
@@ -133,6 +104,80 @@ const book = {
   ]
 }
 
+const investment = {
+  shares: 12000
+}
+
+function getCost({ book, amount, direction }) {
+  if (direction === "buy") {
+    let left = amount
+    let cost = 0
+    for (let i = 0; i < book.asks.length; i++) {
+      const ask = book.asks[i]
+      if (ask.amount < left) {
+        left -= ask.amount
+        cost += ask.amount * ask.price
+      } else {
+        cost += (left * ask.price)
+        break 
+      }
+    }
+    return cost
+  } else {
+    let left = amount
+    let cost = 0
+    for (let i = 0; i < book.bids.length; i++) {
+      const bid = book.bids[i]
+      if (bid.amount < left) {
+        left -= bid.amount
+        cost += bid.amount * bid.price
+      } else {
+        cost += (left * bid.price)
+        break 
+      }
+    }
+    return cost
+  }
+}
+
+function OrderForm () {
+  const [order, setOrder] = useSimpleReducer({price: "", amount: "", direction: "sell", cost: 0})
+
+  useEffect(() => {
+    if (order.amount) {
+      setOrder({ cost: getCost({ book, ...order }) })
+    } else {
+      setOrder({ cost: 0 })
+    }
+  }, [order.amount, order.direction])
+
+  return (
+    <Paper className="OrderForm">
+      <FormControl style={{width: "25%", marginRight: "5%"}}>
+        <InputLabel>Side</InputLabel>
+        <Select value={order.direction}
+          onChange={e => setOrder({ direction: e.target.value })}
+          inputProps={{name: 'Type'}}>
+          <MenuItem value="sell">SELL</MenuItem>
+          <MenuItem value="buy">BUY</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField value={order.amount}
+        style={{width: "70%", marginBottom: "10px"}}
+        onChange={e => setOrder({ amount: e.target.value })}
+        label="Shares"
+        InputProps={{
+          endAdornment: <InputAdornment style={{cursor: "pointer"}} position="end" onClick={() => setOrder({amount: investment.shares})}>max</InputAdornment>,
+        }}
+      />
+      <div className="order-details">
+        <span>Cost: ${nWithCommas(order.cost.toFixed(0))}</span>&nbsp;&nbsp;&nbsp;&nbsp;
+        <Button variant="contained" size="small" color="primary">SUBMIT</Button>
+      </div>
+    </Paper>
+  )
+}
+
 function Book () {
   return (
     <div className="Book">
@@ -143,7 +188,7 @@ function Book () {
             <Table className="Book-bids Book-side">
               <TableHead>
                 <TableRow>
-                  <TableCell className="text-right">Price</TableCell>
+                  <TableCell className="">Price</TableCell>
                   <TableCell className="text-right">Amount</TableCell>
                   <TableCell className="text-right">Value</TableCell>
                   <TableCell></TableCell>
@@ -152,7 +197,7 @@ function Book () {
               <TableBody>
                 {book.bids.map((bid, i) => (
                   <TableRow key={i} className="bid">
-                    <TableCell className="text-right">${bid.price.toFixed(2)}</TableCell>
+                    <TableCell className="price">${bid.price.toFixed(2)}</TableCell>
                     <TableCell className="text-right">{nWithCommas(bid.amount)}</TableCell>
                     <TableCell className="text-right">${nWithCommas(bid.amount * bid.price)}</TableCell>
                     <TableCell className="text-center">
@@ -181,7 +226,7 @@ function Book () {
               <TableBody>
                 {book.asks.map((ask, i) => (
                   <TableRow key={i} className="ask">
-                    <TableCell>${ask.price.toFixed(2)}</TableCell>
+                    <TableCell className="price">${ask.price.toFixed(2)}</TableCell>
                     <TableCell className="text-right">{nWithCommas(ask.amount)}</TableCell>
                     <TableCell className="text-right">${nWithCommas(ask.amount * ask.price)}</TableCell>
                     <TableCell className="text-center">
