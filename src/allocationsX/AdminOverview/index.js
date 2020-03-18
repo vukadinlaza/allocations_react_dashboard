027cdc8f586e77e764eed8e55bd81385a2f60781
Will Sheehan <willsheehan95@gmail.com>
2020-03-18 11:19:27 -0400

    Round out AdminOverview for Exchange with past trades and functioning InputTrade

--- src/allocationsX/AdminOverview/index.js
+++ src/allocationsX/AdminOverview/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import _ from 'lodash'
 import { Row, Col } from 'reactstrap'
 import { useParams, useHistory, useLocation } from 'react-router-dom'
 import { useSimpleReducer } from '../../utils/hooks'
@@ -8,32 +9,230 @@ import { useQuery, useMutation, useLazyQuery } from '@apollo/react-hooks';
 import { Table, TableBody, TableCell, TableRow, TableHead, Paper, Button, TextField, InputAdornment, InputLabel } from '@material-ui/core'
 import Loader from '../../components/utils/Loader'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import UserSearch from '../../components/forms/UserSearch'
+import DealSearch from '../../components/forms/DealSearch'
 
-const MATCH_REQUESTS = gql`
-  query MatchRequests($org: String!) {
-    matchRequests(org: $org) {
-      _id
-      status
-      submitted_at
-      order {
+import "./style.scss"
+
+const EXCHANGE_OVERVIEW = gql`
+  query ExchangeOverview($slug: String!) {
+    organization(slug: $slug) {
+      matchRequests {
         _id
+        status
+        submitted_at
+        deal {
+          company_name
+        }
+        order {
+          _id
+          price
+          amount
+          side
+        }
+        seller {
+          name
+        }
+        buyer {
+          name
+        }
+      }
+      trades {
+        _id
+        buyer { name }
+        seller { name }
         price
         amount
         side
+        settled_at
+        matched_at
+        deal { company_name }
       }
+    }
+  }
+`
+
+const SAVE_TRADE = gql`
+  mutation SaveTrade($org: String!, $trade: TradeInput!) {
+    createTrade(org: $org, trade: $trade) {
+      _id
     }
   }
 `
 
 export default function AdminExchangeOverview () {
   const { organization } = useParams()
-  const { data, error } = useQuery(MATCH_REQUESTS, { variables: { org: organization } })
-
-  console.log({data})
+  const { data, error, refetch } = useQuery(EXCHANGE_OVERVIEW, { variables: { slug: organization } })
 
   return (
     <div className="AdminExchangeOverview">
-      Admin Exchange Overview
+      <Paper className="AdminExchangeOverview" style={{padding: "20px"}}>
+        <Row>
+          <Col sm={{size: 12}}>
+            <h4>Exchange Overview</h4>
+            <hr />
+            <div>
+              <MatchRequests matchRequests={_.get(data, 'organization.matchRequests')} />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="5">
+            <InputTrade refetch={refetch} />
+          </Col>
+          <Col sm="7">
+            <Trades trades={_.get(data, 'organization.trades')} />
+          </Col>
+        </Row>
+      </Paper>
     </div>
+  )
+}
+
+function InputTrade ({ refetch }) {
+  const { organization } = useParams()
+  const [deal, setDeal] = useState(null)
+  const [buyer, setBuyer] = useState(null)
+  const [seller, setSeller] = useState(null)
+  const [trade, setTrade] = useSimpleReducer({ amount: "", price: "", settled_at: "" })
+  const [saveTrade] = useMutation(SAVE_TRADE, {
+    onCompleted: refetch,
+    variables: { org: organization }
+  })
+
+  const submit = () => {
+    // save trade
+    saveTrade({
+      variables: {
+        trade: {
+          ...trade,
+          price: Number(trade.price),
+          amount: Number(trade.amount),
+          seller_id: seller._id,
+          buyer_id: buyer._id,
+          deal_id: deal._id
+        }
+      }
+    })
+  }
+
+  return (
+    <Paper style={{padding: "20px"}}>
+      <h6>Input Completed Trade</h6>
+      <TextField label="shares"
+        style={{width: "100%", marginBottom: "10px"}}
+        variant="outlined"
+        value={trade.amount}
+        onChange={e => setTrade({ amount: e.target.value })} />
+      <TextField label="price"
+        style={{width: "100%", marginBottom: "10px"}}
+        variant="outlined"
+        value={trade.price}
+        onChange={e => setTrade({ price: e.target.value })} />
+      <UserSearch user={buyer} setUser={setBuyer} label="Buyer" showLabelOnSelect />
+      <hr />
+      <UserSearch user={seller} setUser={setSeller} label="Seller" showLabelOnSelect />
+      <hr />
+      <DealSearch deal={deal} setDeal={setDeal} />
+      <hr />
+      <TextField
+        label="Trade Date"
+        variant="outlined"
+        value={trade.settled_at}
+        onChange={e => setTrade({ settled_at: e.target.value })}
+        type="date"
+        style={{width: "70%"}}
+        InputLabelProps={{ shrink: true }}
+      />
+      <Button color="secondary"
+        style={{margin: "10px"}}
+        variant="contained" 
+        onClick={submit}>
+        SAVE
+      </Button>
+    </Paper>
+  )
+}
+
+function MatchRequests ({ matchRequests }) {
+  if (!matchRequests || matchRequests.length === 0) {
+    return (
+      <div className="MatchRequests">
+        <h6>Match Requests</h6>
+        <div>No Outstanding Match Requests</div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="MatchRequests">
+      <h6>Match Requests</h6>
+      <Paper style={{padding: "20px", margin: "10px 0px"}}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Company</TableCell>
+              <TableCell>Shares</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Seller</TableCell>
+              <TableCell>Buyer</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {matchRequests.map(req => (
+              <MatchRequest key={req._id} req={req} />
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
+  )
+}
+
+function MatchRequest ({ req }) {
+  const { order, buyer, seller, deal } = req
+  return (
+    <TableRow key={req._id} className="AdminMatchRequest" style={{padding: "10px", marginBottom: "15px"}}>
+      <TableCell>{deal.company_name}</TableCell>
+      <TableCell>{nWithCommas(order.amount)}</TableCell>
+      <TableCell>${order.price}/share</TableCell>
+      <TableCell>{buyer.name}</TableCell>
+      <TableCell>{seller.name}</TableCell>
+      <TableCell className="text-center">
+        <Button variant="contained" color="primary">Execute</Button>
+      </TableCell>
+    </TableRow>
+  )
+}
+
+function Trades ({ trades = [] }) {
+  return (
+    <Paper padding={"20px"}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Deal</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Shares</TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell>Buyer</TableCell>
+            <TableCell>Seller</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {trades.map(trade => (
+            <TableRow key={trade._id}>
+              <TableCell>{trade.deal.company_name}</TableCell>
+              <TableCell>${trade.price}</TableCell>
+              <TableCell>{nWithCommas(trade.amount)}</TableCell>
+              <TableCell>{trade.settled_at && formatDate(trade.settled_at)}</TableCell>
+              <TableCell>{trade.buyer.name}</TableCell>
+              <TableCell>{trade.seller.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   )
 }
--- src/components/forms/DealSearch.js
+++ src/components/forms/DealSearch.js
@@ -5,32 +5,31 @@ import { useLazyQuery} from '@apollo/react-hooks'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { TextField, Table, TableBody, TableCell, TableRow, Paper } from '@material-ui/core'
 
-export default function UserSearch ({ user, setUser, errors = [], label = "Investor", showLabelOnSelect = false }) {
+export default function DealSearch ({ deal, setDeal, errors = [], label = "Deal" }) {
   const { organization: org } = useParams()
   const [q, setQ] = useState("")
   const [records, setRecords] = useState([])
-  const [search, searchRes] = useLazyQuery(API.users.search)
+  const [search, searchRes] = useLazyQuery(API.deals.searchByOrg)
 
   useEffect(() => {
     search({ variables: { q, org } })
   }, [q])
 
   useEffect(() => {
-    if (searchRes.data && searchRes.data.searchUsers) {
-      setRecords(q === "" ? [] : searchRes.data.searchUsers)
-    } 
+    if (searchRes.data && searchRes.data.searchDealsByOrg) {
+      setRecords(q === "" ? [] : searchRes.data.searchDealsByOrg)
+    }
   }, [searchRes.data])
 
-  if (user) {
+  if (deal) {
     return (
       <Paper className="assoc-value">
         <Table>
           <TableBody>
             <TableRow>
-              {showLabelOnSelect && <TableCell>{label}</TableCell>}
-              <TableCell>{user.name}</TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell><FontAwesomeIcon icon="times" onClick={() => { setQ(""); setUser(null) }} /></TableCell>
+              <TableCell>{deal.company_name}</TableCell>
+              <TableCell>{deal.company_description}</TableCell>
+              <TableCell><FontAwesomeIcon icon="times" onClick={() => { setQ(""); setDeal(null) }} /></TableCell>
             </TableRow>
           </TableBody>
         </Table>
@@ -43,7 +42,7 @@ export default function UserSearch ({ user, setUser, errors = [], label = "Inves
       <TextField style={{width: "100%"}}
         required
         value={q}
-        error={errors.includes("user")}
+        error={errors.includes("deal")}
         label={label}
         variant="filled"
         onChange={e => setQ(e.target.value)} />
@@ -53,9 +52,9 @@ export default function UserSearch ({ user, setUser, errors = [], label = "Inves
             {records.map(record => (
               <TableRow key={record._id} 
                 className="assoc-option" 
-                onClick={() => setUser(record)}>
-                <TableCell>{record.name}</TableCell>
-                <TableCell>{record.email}</TableCell>
+                onClick={() => setDeal(record)}>
+                <TableCell>{record.company_name}</TableCell>
+                <TableCell>{record.company_description}</TableCell>
               </TableRow>
             ))}
           </TableBody>
