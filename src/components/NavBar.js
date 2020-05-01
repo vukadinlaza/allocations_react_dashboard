5d5f8dcde80c88a8cf34c64adc0149e63d9df3cd
Will Sheehan <willsheehan95@gmail.com>
2020-05-01 16:19:46 -0400

    fix logout URL

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,7 +35,7 @@ const NavBar = () => {
 
 function LoginOrProfile () {
   const { loading, user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
-  const logoutWithRedirect = () => logout({ returnTo: "https://allocations.com" });
+  const logoutWithRedirect = () => logout({ returnTo: "https://www.allocations.com" });
 
   if (loading) return <Auth0ProfileLoading /> 
 
