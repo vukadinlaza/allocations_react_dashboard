561988f077bc3a952b8fedeaba1bc6a8437db064
Peer Richelsen <founders@mage.market>
2020-06-08 11:15:02 +0200

    minor changes

--- src/components/admin/AdminHome/index.js
+++ src/components/admin/AdminHome/index.js
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import _ from 'lodash'
-import { useMutation, useQuery } from '@apollo/react-hooks'
-import { gql } from 'apollo-boost'
-import { useAuth } from "../../../auth/useAuth"
-import { useParams, Link } from 'react-router-dom'
-import { nWithCommas, formatDate } from '../../../utils/numbers'
-import { Paper, Table, TableBody, TableCell, TableRow, TableHead, Button, LinearProgress } from '@material-ui/core'
-import { Col, Row } from 'reactstrap'
+import {useMutation, useQuery} from '@apollo/react-hooks'
+import {gql} from 'apollo-boost'
+import {useAuth} from "../../../auth/useAuth"
+import {useParams, Link} from 'react-router-dom'
+import {nWithCommas, formatDate} from '../../../utils/numbers'
+import {Paper, Table, TableBody, TableCell, TableRow, TableHead, Button, LinearProgress} from '@material-ui/core'
+import {Col, Row} from 'reactstrap'
 import Loader from '../../utils/Loader'
-
-import "./style.scss"
+import {makeStyles} from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import {useHistory} from "react-router-dom"
 
 /***
  *
@@ -79,7 +80,19 @@ export const ORG_OVERVIEW = gql`
   }
 `
 
-function sumOrgInvestments ({ investor, deals }) {
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    maxWidth: 800,
+    marginBottom: theme.spacing(4),
+  },
+  divider: {
+    margin: "16px -16px"
+  }
+}));
+
+function sumOrgInvestments({investor, deals}) {
   const deal_ids = deals.map(d => d._id)
   return investor.investments.reduce((acc, inv) => {
     if (deal_ids.includes(inv.deal._id)) {
@@ -89,111 +102,120 @@ function sumOrgInvestments ({ investor, deals }) {
   }, 0)
 }
 
-export default function AdminHome () {
-  const { organization } = useParams()
-  const { data, error, refetch } = useQuery(ORG_OVERVIEW, { 
-    variables: { slug: organization }
+export default function AdminHome() {
+  const classes = useStyles();
+  const history = useHistory()
+
+  const {organization} = useParams()
+  const {data, error, refetch} = useQuery(ORG_OVERVIEW, {
+    variables: {slug: organization}
   })
 
-  if (!data) return <Loader />
+  if (!data) return <Loader/>
 
   const org = data.organization
 
-  const { active, closed } = _.groupBy(org.deals, d => d.status === "closed" ? "closed" : "active")
+  const {active, closed} = _.groupBy(org.deals, d => d.status === "closed" ? "closed" : "active")
 
   return (
-    <div className="AdminHome">
-      {data.investor.admin && <SuperAdmin org={org} />}
-      <Row>
-        <Col sm={{size: 3, offset: 2}}>
-          <Paper className="welcome">
-            <h2>Welcome to, <br></br> <b>{org.name}</b> Admin!</h2> 
+    <>
+      <Grid container>
+        {data.investor.admin && <Grid item xs={12}>
+          <SuperAdmin org={org}/>
+        </Grid>}
+        <Grid item>
+          <Paper>
+            <h2>Welcome to, <br></br> <b>{org.name}</b> Admin!</h2>
             <div>This is where you can manage your deals and investors 🗃 🔮</div>
-            <Button className="create-deal-button" variant="contained">
-              <Link to={`/admin/${organization}/deal/new`}>CREATE DEAL</Link>
+            <Button variant="contained" onClick={() => history.push(`/admin/${organization}/deal/new`)}>
+              Create Deal
             </Button>
           </Paper>
-        </Col>
-        <Col sm={{size: 5, offset: 0}}>
-          <Paper className="deals" style={{padding: "10px 15px"}}>
-            <div className="deals-title">💡 Active Deals &nbsp;<span className="deals-length">{(active || []).length}</span> <Button className="all-btn" variant="contained" color="secondary" style={{padding: "3px 4px"}}><Link to={`/admin/${organization}/deals`}>All</Link></Button></div>
-            <hr></hr>
-            <Paper className="deals-table" style={{marginBottom: "10px"}}>
-              <Table>
-                <TableBody>
-                  {(active || []).map(deal => (
-                    <Deal key={deal._id} deal={deal} />
-                  ))}
-                </TableBody>
-              </Table>
-            </Paper>
-          </Paper>
-        </Col>
-        <Col sm={{size: 4, offset: 2}}>
-          <Paper className="deals-closed" style={{padding: "10px 15px", marginTop: "20px"}}>
-            <div className="deals-title">Closed Deals 🎉 &nbsp;<span className="deals-length">{(closed || []).length}</span> <Button className="all-btn" variant="contained" color="secondary" style={{padding: "3px 4px"}}><Link to={`/admin/${organization}/deals`}>All</Link></Button></div>
-            <hr></hr>
-            <Paper className="deals-table" style={{marginBottom: "10px"}}>
-              <div className="scroll-wrapper">
-                <Table>
-                  <TableBody>
-                    {(closed || []).map(deal => (
-                      <TableRow key={deal._id} className="deal-info">
-                        <TableCell className="company-name">{deal.company_name}</TableCell>
-                        <TableCell>${nWithCommas(deal.amount_raised)}</TableCell>
-                        <TableCell><i>closed {formatDate(deal.date_closed)}</i></TableCell>
-                        <TableCell>
-                          <Link to={`/admin/${organization}/deals/${deal._id}/edit`}>edit</Link>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </div>
-            </Paper>
-          </Paper>
-        </Col>
-        <Col sm={{size: 4, offset: 0}}>
-          <Paper className="investments">
-            <div className="tile-header">Recent Investments 💵 <Button className="all-btn" variant="contained" color="secondary" style={{padding: "3px 4px"}}><Link to={`/admin/${organization}/investments`}>All</Link></Button></div>
-            <hr></hr>
-            <Paper>
-              <div className="scroll-wrapper">
-                <Table>
-                  <TableBody>
-                    {_.take((org.investments || []), 10).filter(i => i.status !== "invited").map(investment => (
-                      <TableRow key={investment._id}>
-                        <TableCell>{investment.deal.company_name}</TableCell>
-                        <TableCell>{_.get(investment, 'investor.name')}</TableCell>
-                        <TableCell>${nWithCommas(investment.amount)}</TableCell>
-                        <TableCell>{investment.status}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </div>
-            </Paper>
-          </Paper>
-        </Col>
-      </Row>
-    </div>
-  )
-}
+        </Grid>
+      </Grid>
 
-function SuperAdmin ({ org }) {
-  return (
-    <Row>
-      <Col sm={{size: 8, offset: 2}}>
-        <Paper className="superadmin-section" style={{marginBottom: "20px", padding: "10px", textAlign: "center"}}>
-          You are a SuperAdmin &nbsp;<Button size="small" variant="contained" color="secondary"><Link to={`/admin/${org.slug}/manager`}>Manage</Link></Button>
+      <Paper className="deals" style={{padding: "10px 15px"}}>
+        <div className="deals-title">💡 Active Deals &nbsp;<span
+          className="deals-length">{(active || []).length}</span> <Button
+          onClick={() => history.push(`/admin/${organization}/deals`)} className="all-btn" variant="contained"
+          color="secondary"
+          style={{padding: "3px 4px"}}>All</Button></div>
+        <Paper className="deals-table" style={{marginBottom: "10px"}}>
+          <Table>
+            <TableBody>
+              {(active || []).map(deal => (
+                <Deal key={deal._id} deal={deal}/>
+              ))}
+            </TableBody>
+          </Table>
         </Paper>
-      </Col>
-    </Row>
+      </Paper>
+      <Paper className="deals-closed" style={{padding: "10px 15px", marginTop: "20px"}}>
+        <div className="deals-title">Closed Deals 🎉 &nbsp;<span
+          className="deals-length">{(closed || []).length}</span>
+          <Button className="all-btn" variant="contained"
+                  color="secondary"
+                  onClick={() => history.push(`/admin/${organization}/deals`)}
+                  style={{padding: "3px 4px"}}>All</Button></div>
+        <Paper className="deals-table" style={{marginBottom: "10px"}}>
+          <div className="scroll-wrapper">
+            <Table>
+              <TableBody>
+                {(closed || []).map(deal => (
+                  <TableRow key={deal._id} className="deal-info">
+                    <TableCell className="company-name">{deal.company_name}</TableCell>
+                    <TableCell>${nWithCommas(deal.amount_raised)}</TableCell>
+                    <TableCell><i>closed {formatDate(deal.date_closed)}</i></TableCell>
+                    <TableCell>
+                      <Link to={`/admin/${organization}/deals/${deal._id}/edit`}>edit</Link>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </Paper>
+      </Paper>
+      <Paper className="investments">
+        <div className="tile-header">Recent Investments
+          💵 <Button onClick={() => history.push(`/admin/${organization}/investments`)} className="all-btn"
+                     variant="contained"
+                     color="secondary"
+                     style={{padding: "3px 4px"}}>
+            All</Button></div>
+        <Paper>
+          <div className="scroll-wrapper">
+            <Table>
+              <TableBody>
+                {_.take((org.investments || []), 10).filter(i => i.status !== "invited").map(investment => (
+                  <TableRow key={investment._id}>
+                    <TableCell>{investment.deal.company_name}</TableCell>
+                    <TableCell>{_.get(investment, 'investor.name')}</TableCell>
+                    <TableCell>${nWithCommas(investment.amount)}</TableCell>
+                    <TableCell>{investment.status}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </Paper>
+      </Paper>
+    </>
   )
 }
 
-function Deal ({ deal }) {
-  const { organization } = useParams()
+function SuperAdmin({org}) {
+  const history = useHistory()
+  return (
+    <Paper className="superadmin-section" style={{marginBottom: "20px", padding: "10px", textAlign: "center"}}>
+      You are a SuperAdmin &nbsp;<Button onClick={() => history.push(`/admin/${org.slug}/manager`)} size="small"
+                                         variant="contained" color="secondary">Manage</Button>
+    </Paper>
+  )
+}
+
+function Deal({deal}) {
+  const {organization} = useParams()
   const val = (Number(deal.amount_raised) / (Number(deal.target) || Infinity)) * 100
 
   return (
@@ -202,7 +224,7 @@ function Deal ({ deal }) {
       <TableCell><i>closes: {deal.date_closed ? formatDate(deal.date_closed) : "tbd"}</i></TableCell>
       <TableCell>
         <div className="text-center">{Math.round(val || 0)}%</div>
-        <LinearProgress className="deal-progress" variant="determinate" color="secondary" value={val} />
+        <LinearProgress className="deal-progress" variant="determinate" color="secondary" value={val}/>
         <div className="text-center">${nWithCommas(deal.amount_raised)} of ${nWithCommas(deal.target)}</div>
       </TableCell>
       <TableCell>
