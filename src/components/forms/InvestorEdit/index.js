552904b1e8c793a400583b1f6e26a256daf4c046
Lance Owen Merrill <lanceomerrill@gmail.com>
2020-06-30 08:46:08 -0700

    Added the accredited_investor_status to the required inputs when profile type is an entitiy

--- src/components/forms/InvestorEdit/index.js
+++ src/components/forms/InvestorEdit/index.js
@@ -94,7 +94,7 @@ export default function InvestorEditForm({investor, setInvestor, actionText, ico
     if (noValidate) return updateInvestor({variables: {investor}})
     
     const validation = validate(investor)
-    const payload = pick(investor, [...reqs, '_id', 'entity_name']);
+    const payload = pick(investor, [...reqs, '_id', 'entity_name', 'accredited_investor_status']);
     setErrors(validation)
     if (validation.length === 0) {
       updateInvestor({variables: { investor: payload }})
