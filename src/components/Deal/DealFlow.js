29bb3381ad619c9be6d12a8e0c05463aace8e153
Lance Owen Merrill <lanceomerrill@gmail.com>
2020-07-06 18:02:12 -0700

    YESSSSSS

--- src/components/Deal/DealFlow.js
+++ src/components/Deal/DealFlow.js
@@ -83,17 +83,6 @@ export default function InvestmentFlow({investment, deal, investor, refetch}) {
   const [status, setStatus] = useState("invited")
   const classes = useStyles();
 
-  useEffect(() => {
-    // if just invited show data room otherwise show pledge page first
-    if (investment) {
-      if (investment.status === "invited") {
-        setStatus("invited")
-      } else {
-        setStatus("pledging")
-      }
-    }
-  }, [investment])
-
   if (!investment) return <Paper style={{padding: "25px"}}><Loader/></Paper>
 
   if (status === "complete") {
@@ -274,7 +263,7 @@ function Pledging({investment, deal, refetch, investor}) {
 
   useEffect(() => {
     setAmount(investment.amount || "")
-  }, [])
+  }, [investment.amount])
 
   const updateAmount = e => {
     const val = e.target.value
@@ -439,7 +428,7 @@ function PledgesViz({deal}) {
         }
       }
     })
-  }, [deal])
+  }, [deal, pledges])
 
   if (!pledges || pledges.length === 0) {
     return (
