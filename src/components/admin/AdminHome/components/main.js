770b348003a3e9a0b34f76151d91cad19bdc3b84
Peer Richelsen <founders@mage.market>
2020-08-10 14:41:20 +0200

    added admin images

--- src/components/admin/AdminHome/components/main.js
+++ src/components/admin/AdminHome/components/main.js
@@ -22,6 +22,10 @@ import OrgCards from './org-cards'
 import ClosedDeals from './closed-deals'
 import Loader from '../../../utils/Loader'
 import Investors from '../../../Investors'
+import NullPaper from "../../../NullPaper";
+import allocations_statement_of_work from "../../../../assets/allocations_statement_of_work.svg";
+import allocations_provisions_of_services from "../../../../assets/allocations_provisions_of_services.svg";
+import allocations_company_profile from "../../../../assets/allocations_company_profile.svg";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -47,8 +51,8 @@ const useStyles = makeStyles((theme) => ({
     width: "100%"
   },
   activeTab: {
-    height: 69,
-    marginTop: 6,
+    height: 75,
+    paddingTop: 6,
     width: "100%",
     borderBottom: "6px solid #205DF5",
     outline: "0 !important",
@@ -64,7 +68,7 @@ const useStyles = makeStyles((theme) => ({
 
 
 export default function OrganizationOverview({orgData, superAdmin}) {
-  const [tab, setTab] = useState("active-deals")
+  const [tab, setTab] = useState("organization");
   const classes = useStyles();
 
   if (!orgData) return <Paper style={{padding: "25px"}}><Loader/></Paper>
@@ -85,29 +89,38 @@ export default function OrganizationOverview({orgData, superAdmin}) {
       </Grid>
 
       <div className={classes.tabs}>
-        <Grid container justify="center">
-          <Grid item xs={12} sm={3}>
+        <Grid container>
+          <Grid item xs={12} sm={2}>
+            <ButtonBase className={tab === "organization" ? classes.activeTab : classes.tab}
+                        onClick={() => setTab('organization')}>
+              Organization
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm={2}>
             <ButtonBase className={tab === "active-deals" ? classes.activeTab : classes.tab}
-                        style={{}}
                         onClick={() => setTab('active-deals')}>
               Active Deals
             </ButtonBase>
           </Grid>
-          <Grid item xs={12} sm={3}>
+          <Grid item xs={12} sm={2}>
             <ButtonBase className={tab === "closed-deals" ? classes.activeTab : classes.tab}
-                        style={{}}
                         onClick={() => setTab('closed-deals')}>
               Closed Deals
             </ButtonBase>
           </Grid>
-          <Grid item xs={12} sm={3}>
+          <Grid item xs={12} sm={2}>
             <ButtonBase className={tab === "all-investors" ? classes.activeTab : classes.tab}
-                        style={{}}
                         onClick={() => setTab('all-investors')}>
               Investors
             </ButtonBase>
           </Grid>
-          <Grid item xs={12} sm={3}>
+          <Grid item xs={12} sm={2}>
+            <ButtonBase className={tab === "all-investors" ? classes.activeTab : classes.tab}
+                        onClick={() => setTab('investments')}>
+              Investments
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm={2}>
             <ButtonBase className={tab === "setting" ? classes.activeTab : classes.tab}
                         style={{borderRight: "1px solid #e1e9ec"}}
                         onClick={() => setTab('setting')}>
@@ -118,6 +131,7 @@ export default function OrganizationOverview({orgData, superAdmin}) {
       </div>
 
       <>
+        {tab === "organization" && <OrganizationPlaceholder/>}
         {tab === "active-deals" && <ActiveDeals orgData={orgData}/>}
         {tab === "closed-deals" && <ClosedDeals orgData={orgData}/>}
         {tab === "profile" && <OrgCards organization={organization} investor={orgData.investor}/>}
@@ -125,4 +139,25 @@ export default function OrganizationOverview({orgData, superAdmin}) {
       </>
     </>
   )
+}
+
+function OrganizationPlaceholder() {
+  return <>
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={12} md={6}>
+        <NullPaper title="Statement of Work" text="TODO: SpaceX SPV" /*TODO Get Company SPV Description here */
+                   image={allocations_statement_of_work} button="Get Started"/>
+      </Grid>
+      <Grid item xs={12} sm={12} md={6}>
+        <NullPaper title="Provision of Services"
+                   text="One click signing for your POS"
+                   image={allocations_provisions_of_services} button="Get Started"/>
+      </Grid>
+      <Grid item xs={12} sm={12} md={6}>
+        <NullPaper title="Company Profile"
+                   text="Finish your company profile" /*TODO Get Company SPV Description here */
+                   image={allocations_company_profile} button="Get Started"/>
+      </Grid>
+    </Grid>
+  </>;
 }
