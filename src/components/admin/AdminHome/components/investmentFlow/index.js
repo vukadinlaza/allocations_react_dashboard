881efe24ae0fa43b38af9ba0bba94259f88f864d
Peer Richelsen <founders@mage.market>
2020-08-13 12:21:39 +0200

    styled kanban

--- src/components/admin/AdminHome/components/investmentFlow/index.js
+++ src/components/admin/AdminHome/components/investmentFlow/index.js
@@ -1,31 +1,47 @@
 import React from 'react'
 import _ from 'lodash'
 import {gql} from 'apollo-boost'
-import {Grid, Paper, Typography, Avatar} from '@material-ui/core'
+import {
+  Grid,
+  Typography,
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  ListItemSecondaryAction
+} from '@material-ui/core'
 import {makeStyles} from "@material-ui/core/styles";
 import {useQuery} from '@apollo/react-hooks'
 import Loader from '../../../../utils/Loader'
-
+import Box from "@material-ui/core/Box";
+import CheckIcon from '@material-ui/icons/Check';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: theme.spacing(2),
-    backgroundColor: "#FFFFFF",
-    minWidth: '100%',
-    minHeight: '400px',
-  },
   divider: {
     margin: "16px -16px"
   },
-  innerPaper: {
-    minWidth: '200px',
-    minHeight: '250px',
+  board: {
+    width: '100%',
     backgroundColor: "##FBFCFC",
     boxShadow: "none",
+    border: "1px solid #70707040",
+    borderRadius: "12px",
   },
   avatar: {
     margin: '0.25rem',
-    backgroundColor: 'blue',
+    height: 26,
+    width: 26,
+    background: "#00A0C6",
+  },
+  list: {
+    padding: "8px"
+  },
+  listItem: {
+    backgroundColor: "#F7F9FA",
+    borderRadius: "12px",
+    marginBottom: 6,
   }
 }));
 
@@ -94,28 +110,51 @@ export default ({deal}) => {
   if (loading) return <Loader/>
 
   return (
-    <Grid container spacing={3}>
+    <Grid container spacing={2} justify="space-between">
       {categories.map((value) => (
-        <Grid key={value.title} item>
-          <Paper className={classes.innerPaper}>
-            <Typography variant="h6" gutterBottom style={{padding: "8px"}}>
+        <Grid key={value.title} item xs={12} sm={3}>
+          <Box height="100%" className={classes.board}>
+            <Typography variant="h6" style={{padding: "8px", textTransform: "uppercase", fontSize: "16px"}}>
               {value.title}
             </Typography>
-            {value?.categoryInvestments?.map(inv => <InvestmentSquare investment={inv}/>)}
-          </Paper>
+            <List dense className={classes.list}>
+              {value?.categoryInvestments?.map(inv => <InvestmentSquare investment={inv}/>)}
+            </List>
+          </Box>
         </Grid>
       ))}
     </Grid>
   )
 }
 
+const checked = true;
 
 const InvestmentSquare = ({investment}) => {
   const classes = useStyles();
   return (
-    <Grid container justify="center" alignItems="center">
-      <Avatar alt={investment.investor.email} className={classes.avatar}/>
-      {investment.investor.email}
-    </Grid>
+    <ListItem className={classes.listItem}>
+      <ListItemAvatar>
+        <Avatar alt={investment.investor.email} className={classes.avatar}>
+          {investment.investor.email.charAt(0).toUpperCase()}
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary={investment.investor.email}/>
+      <ListItemSecondaryAction>
+        {checked &&
+        <CheckIcon style={{
+          color: "#39BE53",
+          position: "absolute",
+          left: 6,
+          top: 6,
+          height: 20,
+          width: 20
+        }}/>}
+        <FiberManualRecordIcon style={{
+          color: "white",
+          height: 32,
+          idth: 32
+        }}/>
+      </ListItemSecondaryAction>
+    </ListItem>
   )
 }
