e460d9c02b412e8509ff1c08a20d3a403061f1d7
Lance Merrill <lanceomerrill@gmail.com>
2020-09-01 10:03:51 -0700

    indentity route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {Redirect, Route, Switch} from "react-router-dom";
-import {ApolloProvider} from '@apollo/react-hooks';
+import { Redirect, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from '@apollo/react-hooks';
 
-import {client} from './apollo-client';
+import { client } from './apollo-client';
 
 import AdminRoute from "./auth/admin-route"
 import PrivateRoute from "./components/PrivateRoute";
@@ -14,6 +14,7 @@ import DealNew from "./components/DealNew";
 import DealEdit from "./components/DealEdit";
 import InvestorEdit from "./components/InvestorEdit";
 import InvestorNew from "./components/InvestorNew";
+import Indentity from './components/Identity'
 import Funds from "./components/Funds";
 import Sidebar from './components/Sidebar';
 import UserHome from './components/UserHome';
@@ -59,55 +60,56 @@ const App = () => {
       {/* Sidebar should handle openState, not App.js */}
       <Sidebar>
         <Switch>
-          <Route path="/cb/thankyou" component={ThankYou} exact/>
+          <Route path="/cb/thankyou" component={ThankYou} exact />
 
-          <PrivateRoute path="/" exact component={UserHome}/>
-          <PrivateRoute path="/profile" component={Profile}/>
-          <PrivateRoute path="/investments" component={UserInvestments}/>
-          <PrivateRoute path="/invited-deals" component={InvitedDeals}/>
+          <PrivateRoute path="/" exact component={UserHome} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/investments" component={UserInvestments} />
+          <PrivateRoute path="/invited-deals" component={InvitedDeals} />
+          <PrivateRoute path="/identity" component={Indentity} />
 
           {/** Onboarding **/}
-          <Route path="/getting-started" component={Faq} exact/>
-          <PrivateRoute path="/spv-onboarding" component={FreeSPVOnboarding} exact/>
+          <Route path="/getting-started" component={Faq} exact />
+          <PrivateRoute path="/spv-onboarding" component={FreeSPVOnboarding} exact />
 
           {/** Deals **/}
-          <Redirect from={`/public/:organization/deals/:deal_slug`} to="/deals/:deal_slug"/>
-          <PrivateRoute path="/deals/:deal_slug" component={Deal} exact/>
-          <PrivateRoute path="/deals/:organization/:deal_slug" component={Deal} exact/>
+          <Redirect from={`/public/:organization/deals/:deal_slug`} to="/deals/:deal_slug" />
+          <PrivateRoute path="/deals/:deal_slug" component={Deal} exact />
+          <PrivateRoute path="/deals/:organization/:deal_slug" component={Deal} exact />
 
           {/** AllocationsX **/}
-          <PrivateRoute path="/exchange" component={AllocationsX} exact/>
-          <PrivateRoute path="/exchange/:deal" component={DealExchange} exact/>
-          <AdminRoute path="/admin/:organization/exchange" component={AdminExchangeOverview} exact/>
+          <PrivateRoute path="/exchange" component={AllocationsX} exact />
+          <PrivateRoute path="/exchange/:deal" component={DealExchange} exact />
+          <AdminRoute path="/admin/:organization/exchange" component={AdminExchangeOverview} exact />
 
-          <AdminRoute path="/investor/:id/home" component={UserHome}/>
-          <AdminRoute path="/investor/:id/investments" component={UserInvestments}/>
-          <AdminRoute path="/investors/new" component={InvestorNew} exact/>
-          <AdminRoute path="/investor/:id/edit" component={InvestorEdit}/>
-          <AdminRoute path="/admin/investment/new" component={InvestmentNew} exact/>
-          <AdminRoute path="/admin/:organization/investments/:id/edit" component={InvestmentEdit} exact/>
-          <AdminRoute path="/admin/organizations/new" component={OrganizationNew} exact/>
+          <AdminRoute path="/investor/:id/home" component={UserHome} />
+          <AdminRoute path="/investor/:id/investments" component={UserInvestments} />
+          <AdminRoute path="/investors/new" component={InvestorNew} exact />
+          <AdminRoute path="/investor/:id/edit" component={InvestorEdit} />
+          <AdminRoute path="/admin/investment/new" component={InvestmentNew} exact />
+          <AdminRoute path="/admin/:organization/investments/:id/edit" component={InvestmentEdit} exact />
+          <AdminRoute path="/admin/organizations/new" component={OrganizationNew} exact />
 
           {/** SuperAdmin **/}
-          <AdminRoute path="/superadmin" component={SuperAdminOverview} exact/>
-          <AdminRoute path="/admin/:organization/manager" component={SuperAdminManager} exact/>
+          <AdminRoute path="/superadmin" component={SuperAdminOverview} exact />
+          <AdminRoute path="/admin/:organization/manager" component={SuperAdminManager} exact />
 
           {/** Whitelabel Routes **/}
-          <PrivateRoute path="/admin/funds" component={Funds} exact/>
-          <PrivateRoute path="/admin/:organization" component={AdminHome} exact/>
-          <AdminRoute path="/admin/:organization/members" component={OrganizationMembers} exact/>
+          <PrivateRoute path="/admin/funds" component={Funds} exact />
+          <PrivateRoute path="/admin/:organization" component={AdminHome} exact />
+          <AdminRoute path="/admin/:organization/members" component={OrganizationMembers} exact />
 
-          <PrivateRoute path="/admin/:organization/deals" component={Deals} exact/>
-          <PrivateRoute path="/admin/:organization/deal/new" component={DealNew} exact/>
-          <PrivateRoute path="/admin/:organization/deals/:id/edit" component={DealEdit} exact/>
+          <PrivateRoute path="/admin/:organization/deals" component={Deals} exact />
+          <PrivateRoute path="/admin/:organization/deal/new" component={DealNew} exact />
+          <PrivateRoute path="/admin/:organization/deals/:id/edit" component={DealEdit} exact />
 
-          <PrivateRoute path="/admin/:organization/investments" component={Investments} exact/>
-          <PrivateRoute path="/admin/:organization/compliance" component={Compliance} exact/>
-          <PrivateRoute path="/admin/:organization/master-filing" component={MasterFiling} exact/>
-          <AdminRoute path="/admin/:organization/investors" component={Investors} exact/>
+          <PrivateRoute path="/admin/:organization/investments" component={Investments} exact />
+          <PrivateRoute path="/admin/:organization/compliance" component={Compliance} exact />
+          <PrivateRoute path="/admin/:organization/master-filing" component={MasterFiling} exact />
+          <AdminRoute path="/admin/:organization/investors" component={Investors} exact />
 
           {/** catchall **/}
-          <PrivateRoute path="/" component={UserHome}/>
+          <PrivateRoute path="/" component={UserHome} />
         </Switch>
       </Sidebar>
     </ApolloProvider>
---
+++ src/components/Identity/index.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react'
+import Loader from '../utils/Loader'
+import { gql } from 'apollo-boost'
+import { pick } from 'lodash'
+import { useAuth } from "../../auth/useAuth";
+import { useMutation } from '@apollo/react-hooks';
+import { toast } from 'react-toastify';
+import {
+    Button
+} from '@material-ui/core'
+import { PassportUploader } from '../forms/InvestorEdit/index'
+
+const GET_INVESTOR = gql`
+  query GetInvestor($email: String, $_id: String) {
+    investor(email: $email, _id: $_id) {
+      _id
+      first_name
+      last_name
+      entity_name
+      country
+      investor_type
+      signer_full_name
+      accredited_investor_status
+      email
+      accredidation_doc {
+        link
+        path
+      }
+      passport {
+        link
+        path
+      }
+    }
+  }
+`
+
+const UPDATE_USER = gql`
+  mutation UpdateUser($investor: UserInput!) {
+    updateUser(input: $investor) {
+      _id
+      first_name
+      last_name
+      country
+      entity_name
+      investor_type
+      signer_full_name
+      accredited_investor_status
+      email
+      passport {
+        link
+        path
+      }
+    }
+  }
+`
+
+const IdentityUpload = () => {
+    const [investor, setInvestor] = useState(null);
+    const [formStatus, setFormStatus] = useState("edit");
+    const [updateInvestor, { data }] = useMutation(UPDATE_USER);
+    const { userProfile, refetch } = useAuth(GET_INVESTOR);
+
+
+    useEffect(() => {
+        if (userProfile) {
+            setInvestor(userProfile)
+        }
+    }, [userProfile])
+
+    useEffect(() => {
+        if (formStatus === "complete") refetch()
+    }, [formStatus])
+
+    const submit = () => {
+        return updateInvestor({
+            variables: {
+                investor: pick(investor, ['_id', 'email', 'passport'])
+            },
+            onCompleted: toast.success('Success')
+        })
+    }
+
+    if (!userProfile.email) return <div><Loader /></div>
+    return (
+        <>
+            <PassportUploader investor={investor} setInvestor={setInvestor} />
+            <Button variant="contained"
+                style={{ marginTop: 16 }}
+                onClick={submit}
+                color="primary">
+                Submit
+            </Button>
+
+        </>
+    )
+}
+
+export default IdentityUpload
