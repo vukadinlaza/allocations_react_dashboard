bb2e71cf64d1e8aff185256ccd07783dd5b59a05
Lance Merrill <lanceomerrill@gmail.com>
2020-09-08 10:03:15 -0700

    swap name to property

--- src/components/Deal/helmet.js
+++ src/components/Deal/helmet.js
@@ -4,9 +4,8 @@ import { Helmet } from "react-helmet";
 const Helm = ({ deal = {} }) => {
     return (
         <Helmet>
-            <meta name="description" content={`${deal.company_description}`} />
+            <meta property="og:description" content={`${deal.company_description}`} />
             <meta name="keywords" content={`SPV, Fund, Funds, ${deal.company_name}`} />
-            <meta name="author" content={deal.deal_lead} />
             <title>{deal.company_name} SPV </title>
         </Helmet>
     );
