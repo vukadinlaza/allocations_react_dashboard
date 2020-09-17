1bab260c443a6a6a684f680283ddd2d70bda612c
Lance Merrill <lanceomerrill@gmail.com>
2020-09-17 09:48:40 -0700

    toggleable form

--- src/components/forms/kycTab.js
+++ src/components/forms/kycTab.js
@@ -4,31 +4,25 @@ import { gql } from 'apollo-boost'
 import { useLazyQuery } from '@apollo/react-hooks';
 import { get } from 'lodash'
 import { useAuth } from "../../auth/useAuth";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Helmet } from "react-helmet";
 import {
   Button,
-  List,
   TextField,
   Paper,
-  Divider,
   Grid,
-  ListItem,
-  ListItemText,
-  ListItemSecondaryAction,
   FormControl,
   InputLabel,
   Select,
   MenuItem,
-  Typography,
-  Checkbox
+  Typography
 } from '@material-ui/core'
 import { makeStyles } from "@material-ui/core/styles";
 
 import countries from "country-region-data"
 import MailingAddress from './kyc/mailing-address-form'
-import TaxTreaty from './kyc/tax-treaty-form'
-import ExtraWNine from './kyc/w9-extra'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -79,6 +73,7 @@ const optional = ['mail_country', 'mail_city', 'mail_zip', 'mail_state', 'mail_s
 export default function DocusignKYCEmbeddedForm({ setLink, deal_slug, org }) {
   const { userProfile } = useAuth(GET_INVESTOR)
   const [investor, setInvestor] = useState({})
+  const [showForm, setShowForm] = useState(false)
   const [getLink, { loading, data }] = useLazyQuery(GET_DOCUSIGN_FORM)
   const [errors, setErrors] = useState([])
   const classes = useStyles();
@@ -139,189 +134,187 @@ export default function DocusignKYCEmbeddedForm({ setLink, deal_slug, org }) {
         <script async src={url}></script>
       </Helmet>
       <form noValidate autoComplete="off">
-        <Typography variant="h6" gutterBottom>
-          KYC Information
+        <Typography variant="h6" gutterBottom style={{ display: 'flex', justifyContent: 'space-between' }} onClick={() => setShowForm(showForm ? false : true)}>
+          KYC Information  <div>{showForm ? <ExpandMoreIcon /> : <ExpandLessIcon />} </div>
         </Typography>
-        <Typography variant="subtitle2">
+        {showForm && <> <Typography variant="subtitle2" style={{ marginBottom: '.5rem' }}>
           This information will only be used to populate your KYC documents.
         </Typography>
+          <Grid container spacing={3}>
 
-        {/* GENERAL INFORMATION */}
-
-
-        <Grid container spacing={3}>
-
-          <Grid item xs={12} sm={12} md={6}>
-            <FormControl required error={errors.includes("investor_type")} variant="outlined" style={{ width: "100%" }}>
-              <InputLabel>Investor Type</InputLabel>
-              <Select value={investor.investor_type || ""}
-                onChange={handleChange("investor_type")}
-                inputProps={{ name: 'Type' }}>
-                <MenuItem value=""></MenuItem>
-                <MenuItem value="individual">Individual</MenuItem>
-                <MenuItem value="entity">Entity</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <FormControl required error={errors.includes("investor_type")} variant="outlined" style={{ width: "100%" }}>
+                <InputLabel>Investor Type</InputLabel>
+                <Select value={investor.investor_type || ""}
+                  onChange={handleChange("investor_type")}
+                  inputProps={{ name: 'Type' }}>
+                  <MenuItem value=""></MenuItem>
+                  <MenuItem value="individual">Individual</MenuItem>
+                  <MenuItem value="entity">Entity</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
 
 
-          <Grid item xs={12} sm={12} md={6}>
-            <FormControl required error={errors.includes("country")} variant="outlined" style={{ width: "100%" }}>
-              <InputLabel>Country of Residence or Place of Business</InputLabel>
-              <Select value={investor.country || ""}
-                onChange={handleChange("country")}
-                inputProps={{ name: 'Country' }}>
-                <MenuItem value=""></MenuItem>
-                {[{ countryName: 'United States' }, ...countries].map(({ countryName }) => (
-                  <MenuItem key={countryName} value={countryName}>{countryName}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <FormControl required disabled error={errors.includes("email")} variant="outlined"
-              style={{ width: "100%" }}>
-              <TextField error={errors.includes("email")}
+            <Grid item xs={12} sm={12} md={6}>
+              <FormControl required error={errors.includes("country")} variant="outlined" style={{ width: "100%" }}>
+                <InputLabel>Country of Residence or Place of Business</InputLabel>
+                <Select value={investor.country || ""}
+                  onChange={handleChange("country")}
+                  inputProps={{ name: 'Country' }}>
+                  <MenuItem value=""></MenuItem>
+                  {[{ countryName: 'United States' }, ...countries].map(({ countryName }) => (
+                    <MenuItem key={countryName} value={countryName}>{countryName}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <FormControl required disabled error={errors.includes("email")} variant="outlined"
+                style={{ width: "100%" }}>
+                <TextField error={errors.includes("email")}
+                  style={{ width: "100%" }}
+                  value={get(investor, 'email') || ""}
+                  onChange={handleChange("email")}
+                  label="Email"
+                  variant="outlined" />
+              </FormControl>
+            </Grid>
+            <InvestorName investor={investor} errors={errors} handleChange={handleChange} />
+            <Grid item xs={12} sm={12} md={6}>
+              <TextField required
+                error={errors.includes("signer_full_name")}
                 style={{ width: "100%" }}
-                value={get(investor, 'email') || ""}
-                onChange={handleChange("email")}
-                label="Email"
+                value={get(investor, 'signer_full_name') || ""}
+                onChange={handleChange("signer_full_name")}
+                label="Full Name of Signer"
                 variant="outlined" />
-            </FormControl>
-          </Grid>
-          <InvestorName investor={investor} errors={errors} handleChange={handleChange} />
-          <Grid item xs={12} sm={12} md={6}>
-            <TextField required
-              error={errors.includes("signer_full_name")}
-              style={{ width: "100%" }}
-              value={get(investor, 'signer_full_name') || ""}
-              onChange={handleChange("signer_full_name")}
-              label="Full Name of Signer"
-              variant="outlined" />
-          </Grid>
+            </Grid>
 
-          <Grid item xs={12} sm={12} md={6}>
-            {investor.investor_type === 'individual' ? <TextField
-              required={investor?.country === 'United States'}
-              error={errors.includes("ssn_itin")}
-              style={{ width: "100%" }}
-              value={get(investor, 'ssn_itin') || ""}
-              onChange={handleChange("ssn_itin")}
-              label="SSN or ITIN"
-              variant="outlined" /> : <TextField
-                required
-                error={errors.includes("ein")}
+            <Grid item xs={12} sm={12} md={6}>
+              {investor.investor_type === 'individual' ? <TextField
+                required={investor?.country === 'United States'}
+                error={errors.includes("ssn_itin")}
                 style={{ width: "100%" }}
-                value={get(investor, 'ein') || ""}
-                onChange={handleChange("ein")}
-                label="EIN"
-                variant="outlined" />}
+                value={get(investor, 'ssn_itin') || ""}
+                onChange={handleChange("ssn_itin")}
+                label="SSN or ITIN"
+                variant="outlined" /> : <TextField
+                  required
+                  error={errors.includes("ein")}
+                  style={{ width: "100%" }}
+                  value={get(investor, 'ein') || ""}
+                  onChange={handleChange("ein")}
+                  label="EIN"
+                  variant="outlined" />}
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <TextField
+                error={errors.includes("foreign_tax_number")}
+                style={{ width: "100%" }}
+                value={get(investor, 'foreign_tax_number') || ""}
+                onChange={handleChange("foreign_tax_number")}
+                label="Foreign Tax Number"
+                variant="outlined" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <TextField
+                error={errors.includes("dob")}
+                style={{ width: "100%" }}
+                value={get(investor, 'dob') || ""}
+                onChange={handleChange("dob")}
+                label="Date Of Birth"
+                variant="outlined"
+                type="date" />
+            </Grid>
+
           </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <TextField
-              error={errors.includes("foreign_tax_number")}
-              style={{ width: "100%" }}
-              value={get(investor, 'foreign_tax_number') || ""}
-              onChange={handleChange("foreign_tax_number")}
-              label="Foreign Tax Number"
-              variant="outlined" />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <TextField
-              error={errors.includes("dob")}
-              style={{ width: "100%" }}
-              value={get(investor, 'dob') || ""}
-              onChange={handleChange("dob")}
-              label="Date Of Birth"
-              variant="outlined"
-              type="date" />
-          </Grid>
-
-        </Grid>
 
 
-        {/* GENERAL ADDRESS */}
+          {/* GENERAL ADDRESS */}
 
-        <Typography variant="subtitle2">
-          Permanent Address
+          <Typography variant="subtitle2">
+            Permanent Address
         </Typography>
 
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={12} md={6}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={12} md={6}>
 
-            <TextField required
-              error={errors.includes("street_address")}
-              style={{ width: "100%" }}
-              value={get(investor, 'street_address') || ""}
-              onChange={handleChange("street_address")}
-              label="Street Address"
-              variant="outlined" />
+              <TextField required
+                error={errors.includes("street_address")}
+                style={{ width: "100%" }}
+                value={get(investor, 'street_address') || ""}
+                onChange={handleChange("street_address")}
+                label="Street Address"
+                variant="outlined" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <TextField required
+                error={errors.includes("city")}
+                style={{ width: "100%" }}
+                value={get(investor, 'city') || ""}
+                onChange={handleChange("city")}
+                label="City"
+                variant="outlined" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <TextField required
+                error={errors.includes("state")}
+                style={{ width: "100%" }}
+                value={get(investor, 'state') || ""}
+                onChange={handleChange("state")}
+                label="State"
+                variant="outlined" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <TextField required
+                error={errors.includes("zip")}
+                style={{ width: "100%" }}
+                value={get(investor, 'zip') || ""}
+                onChange={handleChange("zip")}
+                label="Zip"
+                variant="outlined" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <FormControl required error={errors.includes("address_country")} variant="outlined" style={{ width: "100%" }}>
+                <InputLabel>Country</InputLabel>
+                <Select value={investor.address_country || ""}
+                  onChange={handleChange("address_country")}
+                  inputProps={{ name: 'address-country' }}>
+                  <MenuItem value=""></MenuItem>
+                  {countries.map(({ countryName }) => (
+                    <MenuItem key={countryName} value={countryName}>{countryName}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <TextField required
-              error={errors.includes("city")}
-              style={{ width: "100%" }}
-              value={get(investor, 'city') || ""}
-              onChange={handleChange("city")}
-              label="City"
-              variant="outlined" />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <TextField required
-              error={errors.includes("state")}
-              style={{ width: "100%" }}
-              value={get(investor, 'state') || ""}
-              onChange={handleChange("state")}
-              label="State"
-              variant="outlined" />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <TextField required
-              error={errors.includes("zip")}
-              style={{ width: "100%" }}
-              value={get(investor, 'zip') || ""}
-              onChange={handleChange("zip")}
-              label="Zip"
-              variant="outlined" />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <FormControl required error={errors.includes("address_country")} variant="outlined" style={{ width: "100%" }}>
-              <InputLabel>Country</InputLabel>
-              <Select value={investor.address_country || ""}
-                onChange={handleChange("address_country")}
-                inputProps={{ name: 'address-country' }}>
-                <MenuItem value=""></MenuItem>
-                {countries.map(({ countryName }) => (
-                  <MenuItem key={countryName} value={countryName}>{countryName}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-        </Grid>
 
-        {/* MAILING */}
-        {investor.country !== 'United States' && <>
+          {/* MAILING */}
+          {investor.country !== 'United States' && <>
 
-          <MailingAddress investor={investor} setInvestor={setInvestor} handleChange={handleChange} errors={errors} />
-        </>}
+            <MailingAddress investor={investor} setInvestor={setInvestor} handleChange={handleChange} errors={errors} />
+          </>}
 
-        <Button variant="contained"
-          onClick={submit}
-          color="primary">
-          Next
+          <Button variant="contained"
+            onClick={submit}
+            style={{ marginTop: '1rem' }}
+            color="primary">
+            Next
         </Button>
-
+        </>
+        }
       </form>
-      <hr/>
-      <div style={{ marginTop: "1rem", paddingTop:"1rem", paddingBottom:"1rem" }}>
+      <hr />
+      <div style={{ marginTop: "1rem", paddingTop: "1rem", paddingBottom: "1rem" }}>
         <p>Verify your accredited investor status with VerifyInvestor.</p>
-          <Button id="invest" variant="contained" color="secondary" onClick={() => {
-              const token = "4YY9eiTQJrfxNieMIR-quA";
-              const identifier = "Allocations"; // optional
-              const portal_name = "Test_Allocations"; // optional
-              const deal_name = "Test Deal"; // optional
-              window.verifyInvestor(token, identifier, portal_name, deal_name);
-          }} >Verify Accredited Investor Status</Button>
+        <Button id="invest" variant="contained" color="secondary" onClick={() => {
+          const token = "4YY9eiTQJrfxNieMIR-quA";
+          const identifier = "Allocations"; // optional
+          const portal_name = "Test_Allocations"; // optional
+          const deal_name = "Test Deal"; // optional
+          window.verifyInvestor(token, identifier, portal_name, deal_name);
+        }} >Verify Accredited Investor Status</Button>
       </div>
     </>
   )
