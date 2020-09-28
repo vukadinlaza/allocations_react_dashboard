70ca9ea679ecc28cf50f2a6a3d844562b6e506e6
Lance Owen Merrill <40847460+Zilula@users.noreply.github.com>
2020-09-28 12:18:20 -0700

    fund overview (#190)
    * fund overview
    
    * add fund data

---
+++ src/components/admin/AdminHome/fundData.js
@@ -0,0 +1,110 @@
+const data = [
+    {
+        slug: 'browder-capital', data: [{
+            Date: '17 Aug 2020',
+            Source: 'Spend Money',
+            Description: 'PLACEPULL CORP - PLACEPULL CORP O0R2Q Domestic Manual Wire',
+            Debit: '25,000.00',
+            'Running Balance': '25,000.00',
+            Gross: '25,000.00',
+            'Private Funds': 'Browder Capital LP I - Browder Capital',
+        },
+        {
+            Date: '20 Aug 2020',
+            Source: 'Spend Money',
+            Description: 'Allocations, Inc. - SPV Fees',
+            Debit: '50,000.00',
+            'Running Balance': '75,000.00',
+            Gross: '50,000.00',
+            'Private Funds': 'Browder Capital LP I - Browder Capital',
+        },
+        {
+            Date: '27 Aug 2020',
+            Source: 'Spend Money',
+            Description: 'REMATTER INC - REMATTER INC P1T23 Domestic Manual Wire',
+            Debit: '10,000.00',
+            'Running Balance': '85,000.00',
+            Gross: '10,000.00',
+            'Private Funds': 'Browder Capital LP I - Browder Capital',
+        },
+        {
+            Date: '02 Sep 2020',
+            Source: 'Spend Money',
+            Description: 'MESH DIGITAL SERVICES INC. - MESH DIGITAL SERVICES INC. O2C1A Domestic Manual Wire',
+            Debit: '50,000.00',
+            'Running Balance': '135,000.00',
+            Gross: '50,000.00',
+            'Private Funds': 'Browder Capital LP I - Browder Capital',
+        },
+        {
+            Date: '02 Sep 2020',
+            Source: 'Spend Money',
+            Description: 'YOTTA TECHNOLOGIES INC - YOTTA TECHNOLOGIES INC O0H04 Domestic Manual Wire',
+            Debit: '25,000.00',
+            'Running Balance': '160,000.00',
+            Gross: '25,000.00',
+            'Private Funds': 'Browder Capital LP I - Browder Capital',
+        },
+        {
+            Date: '08 Sep 2020',
+            Source: 'Spend Money',
+            Description: 'Allocations, Inc. - SPV Fees',
+            Debit: '50,000.00',
+            'Running Balance': '210,000.00',
+            Gross: '50,000.00',
+            'Private Funds': 'Browder Capital LP I - Browder Capital',
+        },
+        {
+            Date: '14 Sep 2020',
+            Source: 'Spend Money',
+            Description: 'AVRO LIFE SCIENCE INC - AVRO LIFE SCIENCE INC W2D1U Domestic Online Wire',
+            Debit: '50,000.00',
+            'Running Balance': '260,000.00',
+            Gross: '50,000.00',
+            'Private Funds': 'Browder Capital LP I - Browder Capital',
+        },
+        {
+            Date: '18 Sep 2020',
+            Source: 'Spend Money',
+            Description: 'BRANCH TECHNOLOGIES INC. - BRANCH TECHNOLOGIES INC. U1Z1T Domestic Online Wire',
+            Debit: '100,000.00',
+            'Running Balance': '360,000.00',
+            Gross: '100,000.00',
+            'Private Funds': 'Browder Capital LP I - Browder Capital',
+        },
+        {
+            Date: '18 Sep 2020',
+            Source: 'Spend Money',
+            Description: 'COMMSOR INC - COMMSOR INC X032P Domestic Online Wire',
+            Debit: '25,000.00',
+            'Running Balance': '385,000.00',
+            Gross: '25,000.00',
+            'Private Funds': 'Browder Capital LP I - Browder Capital',
+        },
+        {
+            Date: '23 Sep 2020',
+            Source: 'Spend Money',
+            Description: 'OFFSIGHT INC - OFFSIGHT INC 42605 Domestic Online Wire',
+            Debit: '100,000.00',
+            'Running Balance': '485,000.00',
+            Gross: '100,000.00',
+            'Private Funds': 'Browder Capital LP I - Browder Capital',
+        }
+        ]
+    },
+    {
+        slug: 'theventurecollective', data: [
+            {
+                Date: 'Total TVC I - Axiom Space',
+                Source: 'Spend Money',
+                Description: 'Axiom Space - AXIOM SPACE INC C0N2R Domestic Manual Wire',
+                Debit: '2,281,565.00',
+                'Running Balance': '2,281,565.00',
+                Gross: '2,281,565.00',
+                'Private Funds': 'TVC I - Axiom Space'
+            }
+        ]
+    }
+]
+
+export default data
---
+++ src/components/admin/AdminHome/fundOverview.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { gql } from 'apollo-boost'
+
+import {
+    Paper,
+    Grid,
+} from '@material-ui/core';
+import _ from 'lodash'
+import Chart from "react-google-charts"
+import { useHistory } from "react-router-dom"
+import { nWithCommas } from '../../../utils/numbers'
+
+export default ({ data, children }) => {
+    const history = useHistory()
+
+    const chartEvents = [
+        {
+            eventName: "select",
+            callback({ chartWrapper }) {
+                history.push(`/investments`)
+            }
+        }
+    ];
+
+    const chartOptionsA = {
+        title: '',
+        pieHole: 0.5,
+    };
+    const chartOptionsB = {
+        title: 'The decline of \'The 39 Steps\'',
+        vAxis: { title: 'Accumulated Rating' },
+        isStacked: true
+    };
+    return (
+        <div style={{
+            height: "430px",
+            background: "#005EFF",
+            marginTop: "-30px",
+            paddingTop: "30px",
+            paddingBottom: "60px",
+            marginLeft: "-32px",
+            paddingLeft: "32px",
+            marginRight: "-32px",
+            paddingRight: "32px"
+        }}>
+            {children}
+
+            <Grid container justify="space-between" style={{ marginTop: "40px" }}>
+                <Grid item sm={12} md={4} style={{ border: "1em solid transparent" }}>
+                    <Paper style={{ minHeight: "100px" }}>
+                        <Grid container>
+                            <Grid item sm={8} md={8}>
+                                <p style={{ color: "rgba(0,0,0,0.4)", paddingLeft: "10px", paddingTop: "10px" }}>Portfolio Value</p>
+                                <h2 align="left" style={{ color: "rgba(0,0,0,0.8)", paddingLeft: "10px" }}>{nWithCommas(_.sumBy(data.map(inv => ({ ...inv, debit: _.toNumber(inv.Debit.replaceAll(',', '')) })), 'debit'))}</h2>
+                                <p style={{ color: "rgba(0,0,0,0.4)", paddingLeft: "10px", paddingTop: "10px" }}>0% realized | 100% unrealized</p>
+                            </Grid>
+                            <Grid item sm={4} md={4} justify="center" align="center">
+                                <img src="https://allocations-public.s3.us-east-2.amazonaws.com/money-sign.png" alt="oops" style={{ width: "50px", height: "50px", marginTop: "30%" }} />
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+                <Grid item sm={12} md={4} style={{ border: "1em solid transparent" }}>
+                    <Paper style={{ minHeight: "100px" }}>
+                        <Grid item sm={8} md={8}>
+                            <p style={{ color: "rgba(0,0,0,0.4)", paddingLeft: "10px", paddingTop: "10px" }}>Total Invested</p>
+                            <h2 align="left" style={{ color: "rgba(0,0,0,0.8)", paddingLeft: "10px" }}>{nWithCommas(_.sumBy(data.map(inv => ({ ...inv, debit: _.toNumber(inv.Debit.replaceAll(',', '')) })), 'debit'))}</h2>
+                            <p style={{ color: "rgba(0,0,0,0.4)", paddingLeft: "10px", paddingTop: "10px" }}>{data.length} Total Investments</p>
+                        </Grid>
+                        <Grid item sm={4} md={4}>
+
+                        </Grid>
+                    </Paper>
+                </Grid>
+                <Grid item sm={12} md={4} style={{ border: "1em solid transparent" }}>
+                    <Paper style={{ minHeight: "100px" }}>
+                        <Grid item sm={8} md={8}>
+                            <p style={{ color: "rgba(0,0,0,0.4)", paddingLeft: "10px", paddingTop: "10px" }}>Multiple</p>
+                            <h2 align="left" style={{ color: "rgba(0,0,0,0.8)", paddingLeft: "10px" }}>1</h2>
+                            <p style={{ color: "rgba(0,0,0,0.4)", paddingLeft: "10px", paddingTop: "10px" }}>1</p>
+                        </Grid>
+                        <Grid item sm={4} md={4}>
+
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
+
+            <Grid container justify="space-between" style={{ marginTop: "1em" }}>
+                <Grid item sm={12} md={6} style={{ border: "1em solid transparent" }}>
+                    <Paper style={{ minHeight: "400px" }}>
+                        <p style={{ color: "rgba(0,0,0,0.4)", paddingLeft: "10px", paddingTop: "10px" }}>Overview</p>
+                        <h6 style={{ color: "rgba(0,0,0,0.4)", paddingLeft: "10px", paddingTop: "0px" }}>Portfolio Management</h6>
+                        <Grid item sm={12} md={12}>
+                            <Chart chartType="PieChart"
+                                width="100%"
+                                height="300px"
+                                chartEvents={chartEvents}
+                                data={[['Investment', 'Amount'], ...data.map(investment => ([investment.Description.split('-')[0], _.toNumber(investment.Debit.replaceAll(',', ''))]))]}
+                                options={chartOptionsA} />
+                        </Grid>
+                    </Paper>
+                </Grid>
+                <Grid item sm={12} md={6} style={{ border: "1em solid transparent" }}>
+                    <Paper style={{ minHeight: "400px" }}>
+                        <p style={{ color: "rgba(0,0,0,0.4)", paddingLeft: "5px" }}>Unrealized vs Realized</p>
+                        <Grid item sm={12} md={12}>
+                            <Chart chartType="SteppedAreaChart"
+                                width="100%"
+                                height="300px"
+                                chartEvents={chartEvents}
+                                data={[['Amount', 'Unrealized', 'Realized'], ['Amount', _.sumBy(data.map(inv => ({ ...inv, debit: _.toNumber(inv.Debit.replaceAll(',', '')) })), 'debit'), 0]]}
+                                optionsA={chartOptionsB} />
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
+
+        </div>
+    )
+}
