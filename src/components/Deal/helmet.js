933511c9fe1114eaf2f6ac173a76bb3e3248017c
Lance Merrill <lanceomerrill@gmail.com>
2020-10-15 11:11:26 -0700

    unfurl

--- src/components/Deal/helmet.js
+++ src/components/Deal/helmet.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { Helmet } from "react-helmet";
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const Helm = ({ deal = {} }) => {
-    return (
-        <Helmet>
-            <meta property="og:description" content={`${deal.company_description}`} />
-            <meta name="keywords" content={`SPV, Fund, Funds, ${deal.company_name}`} />
-            <title>{deal.company_name}</title>
-        </Helmet>
-    );
+  return (
+    <Helmet>
+      <meta
+        name="keywords"
+        content={`SPV, Fund, Funds, ${deal.company_name}`}
+      />
+      <title>{deal.company_name}</title>
+      <meta property="og:title" content={`${deal.company_name}`} />
+      <meta property="og:description" content={`${deal.company_description}`} />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content="https://allocations-public.s3.us-east-2.amazonaws.com/allocations-logo.svg"
+      />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="500" />
+    </Helmet>
+  );
 };
 
-export default Helm
+export default Helm;
