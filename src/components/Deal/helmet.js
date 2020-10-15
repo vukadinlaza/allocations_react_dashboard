3236e9f9b6140b8705a86ccac9f786841501a68f
Lance Merrill <lanceomerrill@gmail.com>
2020-10-15 13:18:13 -0700

    update fund, swap in allocations logo in unfurl and updated eslint

--- src/components/Deal/helmet.js
+++ src/components/Deal/helmet.js
@@ -4,18 +4,12 @@ import { Helmet } from 'react-helmet';
 const Helm = ({ deal = {} }) => {
   return (
     <Helmet>
-      <meta
-        name="keywords"
-        content={`SPV, Fund, Funds, ${deal.company_name}`}
-      />
+      <meta name="keywords" content={`SPV, Fund, Funds, ${deal.company_name}`} />
       <title>{deal.company_name}</title>
       <meta property="og:title" content={`${deal.company_name}`} />
       <meta property="og:description" content={`${deal.company_description}`} />
       <meta property="og:type" content="website" />
-      <meta
-        property="og:image"
-        content="https://allocations-public.s3.us-east-2.amazonaws.com/allocations-logo.svg"
-      />
+      <meta property="og:image" content="https://allocations-public.s3.us-east-2.amazonaws.com/allocations-logo.svg" />
       <meta property="og:image:width" content="400" />
       <meta property="og:image:height" content="500" />
     </Helmet>
