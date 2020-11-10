4541bdfd2fbd2f593cab9009712100a825432d22
Lance Merrill <lanceomerrill@gmail.com>
2020-11-10 09:17:00 -0800

    updates

--- src/components/Build/sign.js
+++ src/components/Build/sign.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default ({ deal }) => {
+const powerFormLink =
+  process.env === 'production'
+    ? 'https://na3.docusign.net/Member/PowerFormSigning.aspx?PowerFormId=4967a16a-d608-4192-a473-5f2a54f9e330&env=na3&acct=5ff4424d-446e-45ab-a456-3382543498de&v=2&'
+    : 'https://demo.docusign.net/Member/PowerFormSigning.aspx?PowerFormId=3655160a-bd2f-4e8f-8748-ffedb4494cd1&env=demo&acct=26630525-c754-47e0-a821-a9ca4134ac03&v=2&';
+export default ({ deal, user }) => {
   const params = {
+    [`signer_Email`]: user.email,
+    [`signer_UserName`]: user.first_name,
     [`build-airtable-id`]: deal.airtableId,
     [`build-carried-interest-other-fees`]: `Organizer Managment Fee Amount: ${
       deal.org_charge_mgmt_fee_amount || 0
@@ -16,7 +22,7 @@ export default ({ deal }) => {
   const urlParameters = Object.entries(params)
     .map((e) => e.map(encodeURI).join('='))
     .join('&');
-  const link = `https://demo.docusign.net/Member/PowerFormSigning.aspx?PowerFormId=3655160a-bd2f-4e8f-8748-ffedb4494cd1&env=demo&acct=26630525-c754-47e0-a821-a9ca4134ac03&v=2&${urlParameters}`;
+  const link = `${powerFormLink}${urlParameters}`;
   return (
     <div>
       <div className="external-sign-link">
