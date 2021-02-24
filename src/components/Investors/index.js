a56a7e749071e7bbf81c68408784744b887ef10e
Lance Merrill <lanceomerrill@gmail.com>
2021-02-24 08:59:32 -0800

    view dashboard as

--- src/components/Investors/index.js
+++ src/components/Investors/index.js
@@ -1,24 +1,23 @@
-import React, { useEffect } from 'react'
-import _ from 'lodash'
+import React, { useEffect } from 'react';
+import _ from 'lodash';
 import { Link, useParams } from 'react-router-dom';
-import { gql } from 'apollo-boost'
+import { gql } from 'apollo-boost';
 import { useLazyQuery } from '@apollo/react-hooks';
-import { useAuth } from "../../auth/useAuth";
-import { Row, Col } from 'reactstrap'
-import { nWithCommas } from '../../utils/numbers'
-import Loader from "../utils/Loader"
+import { Row, Col } from 'reactstrap';
+import { Table, TableBody, TableCell, TableRow, TableHead, Paper, Button, Grid } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import { useAuth } from '../../auth/useAuth';
+import { nWithCommas } from '../../utils/numbers';
+import Loader from '../utils/Loader';
 import { getDisplayName } from '../../utils/displayName';
 
-import { Table, TableBody, TableCell, TableRow, TableHead, Paper, Button, Grid } from '@material-ui/core'
+import './style.scss';
 
-import "./style.scss";
-import Typography from "@material-ui/core/Typography";
-
-/***
+/** *
  *
  * All investors in table view for superadmins
  *
- **/
+ * */
 
 const GET_INVESTORS = gql`
   query GetOrg($slug: String!) {
@@ -43,26 +42,38 @@ const GET_INVESTORS = gql`
       }
     }
   }
-`
+`;
 
 export default function Investors() {
-  const { organization } = useParams()
-  const { userProfile } = useAuth()
-  const [getInvestors, { data, error }] = useLazyQuery(GET_INVESTORS, { variables: { slug: organization } })
+  const { organization } = useParams();
+  const { userProfile } = useAuth();
+  const [getInvestors, { data, error }] = useLazyQuery(GET_INVESTORS, { variables: { slug: organization } });
 
   useEffect(() => {
-    if (userProfile && userProfile.email) getInvestors()
-  }, [userProfile])
+    if (userProfile && userProfile.email) getInvestors();
+  }, [getInvestors, userProfile]);
 
-  if (error) return <div>{error.message}</div>
+  if (error) return <div>{error.message}</div>;
 
-  if (!data?.organization?.deals) return <div><Loader /></div>
+  if (!data?.organization?.deals)
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
 
-  const { organization: { deals } } = data
-  const investors = _.uniq(deals.reduce((acc, deal) => {
-    const investors = deal.investments.map(investment => investment.investor)
-    return [...acc, ...investors]
-  }, []).filter(inv => inv), 'email')
+  const {
+    organization: { deals },
+  } = data;
+  const investors = _.uniq(
+    deals
+      .reduce((acc, deal) => {
+        const investors = deal.investments.map((investment) => investment.investor);
+        return [...acc, ...investors];
+      }, [])
+      .filter((inv) => inv),
+    'email',
+  );
 
   return (
     <div className="Investors">
@@ -76,13 +87,17 @@ export default function Investors() {
       <Grid container>
         <Grid item xs={12}>
           <Paper className="table-wrapper">
-            <Grid container xs={12} justify="space-between" style={{ padding: "16px" }}>
+            <Grid container xs={12} justify="space-between" style={{ padding: '16px' }}>
               <Typography variant="h6" gutterBottom>
                 Investors
-            </Typography>
-              {organization === "allocations" && <Link to="/investors/new">
-                <Button variant="contained" color="secondary">INVITE INVESTOR</Button>
-              </Link>}
+              </Typography>
+              {organization === 'allocations' && (
+                <Link to="/investors/new">
+                  <Button variant="contained" color="secondary">
+                    INVITE INVESTOR
+                  </Button>
+                </Link>
+              )}
             </Grid>
             <Table>
               <TableHead>
@@ -91,25 +106,35 @@ export default function Investors() {
                   <TableCell>Email</TableCell>
                   <TableCell>Investments</TableCell>
                   <TableCell>Total Invested</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
+                  <TableCell />
+                  <TableCell />
                 </TableRow>
               </TableHead>
               <TableBody>
-                {_.orderBy(investors, ({ investments }) => _.sumBy(investments, 'amount'), 'desc').map(investor => (
+                {_.orderBy(investors, ({ investments }) => _.sumBy(investments, 'amount'), 'desc').map((investor) => (
                   <TableRow key={investor._id}>
                     <TableCell>{investor.email}</TableCell>
                     <TableCell>{investor.email}</TableCell>
-                    <TableCell>{investor.investments.filter(inv => inv.organization === data?.organization?._id).length}</TableCell>
-                    <TableCell>${nWithCommas(_.sumBy(investor.investments.filter(inv => inv.organization === data?.organization?._id), 'amount'))}</TableCell>
                     <TableCell>
-                      {organization === "allocations" && <Link to={`/investor/${investor._id}/home`}>
-                        Use site as {investor.first_name || investor.entity_name}
-                      </Link>
-                      }
+                      {investor.investments.filter((inv) => inv.organization === data?.organization?._id).length}
                     </TableCell>
                     <TableCell>
+                      $
+                      {nWithCommas(
+                        _.sumBy(
+                          investor.investments.filter((inv) => inv.organization === data?.organization?._id),
+                          'amount',
+                        ),
+                      )}
                     </TableCell>
+                    <TableCell>
+                      {(organization === 'allocations' || organization === 'vitalize') && (
+                        <Link to={`/investor/${investor._id}/home`} target="_blank">
+                          View dashboard as {investor.first_name || investor.entity_name}
+                        </Link>
+                      )}
+                    </TableCell>
+                    <TableCell />
                   </TableRow>
                 ))}
               </TableBody>
@@ -118,5 +143,5 @@ export default function Investors() {
         </Grid>
       </Grid>
     </div>
-  )
+  );
 }
