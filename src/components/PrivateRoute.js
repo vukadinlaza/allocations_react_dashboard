e2dd655782768f3d23044d42b3d21040841eadc5
Lance Merrill <lanceomerrill@gmail.com>
2021-03-18 12:08:01 -0700

    fix private route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Route, useLocation } from 'react-router-dom';
 import { withAuthenticationRequired } from '@auth0/auth0-react';
 import queryString from 'query-string';
-
+import base64 from 'base-64';
 import Loader from './utils/Loader';
 
 const connection = process.env.NODE_ENV === 'production' ? 'theventurecollective' : 'tvc';
 const PrivateRoute = ({ component, ...args }) => {
-  const location = useLocation();
-  const query = queryString.parse(location.search);
+  const { search, pathname } = useLocation();
+  const isTvc = pathname.includes('theventurecollective');
+  let decodedParams = {};
+  if (isTvc) {
+    decodedParams = base64.decode(search.substring(1));
+  }
+  const paramsToUse = isTvc ? decodedParams : search;
+  const query = queryString.parse(paramsToUse);
+
   const tvc = query.login === 'tvc' ? connection : '';
+  console.log('IS TVC from private route', tvc);
   return (
     <Route
       component={withAuthenticationRequired(component, {
