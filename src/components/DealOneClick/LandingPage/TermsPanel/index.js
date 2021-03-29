40865c2c7a2ec7280f90c4d431d69ebcdffb6714
Danny Hogan <hogan.daniel@hotmail.com>
2021-03-29 14:22:29 -0700

    max investment

--- src/components/DealOneClick/LandingPage/TermsPanel/index.js
+++ src/components/DealOneClick/LandingPage/TermsPanel/index.js
@@ -3,21 +3,22 @@ import './styles.scss'
 
 const TermsPanel = ({ deal }) => {
 
-  console.log(deal)
+  const { deal_lead, maximumInvestment } = deal
+
   return (
     <section className="TermsPanel">
-    <p className="section-label">Terms</p>
-    <ul>
-      <li>
-        <p>Deal lead:</p>
-        <h3>Kingsley Advani</h3>
-      </li>
-      <li>
-        <p>Maximum investment:</p>
-        <h3>$100,000.00</h3>
-      </li>
-    </ul>
-  </section>
+      <p className="section-label">Terms</p>
+      <ul>
+        <li>
+          <p>Deal lead:</p>
+          <h3>{deal_lead}</h3>
+        </li>
+        <li>
+          <p>Maximum investment:</p>
+          <h3>{maximumInvestment ? maximumInvestment : 'No maximum investment amount set.'}</h3>
+        </li>
+      </ul>
+    </section>
   )
 }
 
