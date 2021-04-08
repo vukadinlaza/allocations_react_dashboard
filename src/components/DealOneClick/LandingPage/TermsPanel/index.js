fe77dff48c8998500bf2045dc090f7741eeed064
Lance Merrill <lanceomerrill@gmail.com>
2021-04-08 07:43:48 -0700

    fix dealterms

--- src/components/DealOneClick/LandingPage/TermsPanel/index.js
+++ src/components/DealOneClick/LandingPage/TermsPanel/index.js
@@ -1,33 +1,46 @@
-import React from 'react'
-import './styles.scss'
+import React from 'react';
+import './styles.scss';
 
 const TermsPanel = ({ deal }) => {
+  const {
+    deal_lead,
+    dealParams: { totalCarry, managementFees, spvName, fundGeneralPartner, fundTotalCarry, fundManagementFees },
+  } = deal;
 
-  const { deal_lead, dealParams: { totalCarry, managementFees, spvName } } = deal
-
+  const dealLead = fundGeneralPartner || deal_lead || '';
+  const carry = fundTotalCarry || totalCarry || '';
+  const fees = fundManagementFees || managementFees || '';
   return (
     <section className="TermsPanel">
       <p className="section-label">Terms</p>
       <ul>
-        <li>
-          <p>Manager:</p>
-          <h3>{deal_lead ? deal_lead : 'No deal manager specified.'}</h3>
-        </li>
-        <li>
-          <p>SPV Name:</p>
-          <h3>{spvName ? spvName : 'Not specified.'}</h3>
-        </li>
-        <li>
-          <p>Total carry:</p>
-          <h3>{totalCarry ? totalCarry + '%' : 'Not specified.'}</h3>
-        </li>
-        <li>
-          <p>Total management fee:</p>
-          <h3>{managementFees ? managementFees + '%' : 'Not specified.'}</h3>
-        </li>
+        {dealLead && (
+          <li>
+            <p>Manager:</p>
+            <h3>{dealLead}</h3>
+          </li>
+        )}
+        {spvName && (
+          <li>
+            <p>SPV Name:</p>
+            <h3>{spvName}</h3>
+          </li>
+        )}
+        {carry && (
+          <li>
+            <p>Total carry:</p>
+            <h3>{carry}%</h3>
+          </li>
+        )}
+        {fees && (
+          <li>
+            <p>Total management fee:</p>
+            <h3>{fees}%</h3>
+          </li>
+        )}
       </ul>
     </section>
-  )
-}
+  );
+};
 
 export default TermsPanel;
