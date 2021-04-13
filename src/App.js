fcecb736991f269bbbe36dd4b6534130db163867
Lance Merrill <lanceomerrill@gmail.com>
2021-04-13 14:24:33 -0700

    many welps

--- src/App.js
+++ src/App.js
@@ -91,18 +91,17 @@ const App = () => {
           {/** Deals * */}
           {/* PUBLIC Landing Page */}
           <Route path="/public/:organization/:deal_slug" component={DealLandingPage} exact />
+          <Route path="/public/:deal_slug" component={DealLandingPage} exact />
 
           {/* Private Landing Page */}
           <PrivateRoute path="/deals/:deal_slug" component={DealLandingPage} exact />
           <PrivateRoute path="/deals/:organization/:deal_slug" component={DealLandingPage} exact />
 
           {/* Private Invest Page */}
-
           <PrivateRoute path="/invest/:deal_slug" component={InvestmentPage} exact />
           <PrivateRoute path="/invest/:organization/:deal_slug" component={InvestmentPage} exact />
 
           {/* Private Next Steps page */}
-
           <PrivateRoute path="/next-steps/:deal_slug" component={DealNextSteps} exact />
           <PrivateRoute path="/next-steps/:organization/:deal_slug" component={DealNextSteps} exact />
 
