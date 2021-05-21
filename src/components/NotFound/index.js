8ad61034edb606645857e12b7583cd2e0efdb6be
EmNicholson93 <Eli.m.nicholson@gmail.com>
2021-05-21 12:40:59 -0700

    minor cleanup, renamed file for consistency

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import './styles.scss';
+import './style.scss';
 import { useHistory } from 'react-router-dom';
-import { Button, Grid } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import rocket from '../../assets/rocket.png';
+
 export default function NotFound() {
 
   const history = useHistory();
@@ -13,8 +14,12 @@ export default function NotFound() {
       <h1 className="err-header">
         Oh no! 404 error.
       </h1>
-      <div className="err-text">The page was not found. Let's take your rocket back to safety.</div>
-      <Button className="err-button" onClick={() => history.push(`/`)} variant="contained" color="primary">Go to homepage</Button>
+      <div className="err-text">
+        The page was not found. Let's take your rocket back to safety.
+      </div>
+      <Button className="err-button" onClick={() => history.push(`/`)} variant="contained" color="primary">
+        Go to homepage
+      </Button>
     </div>
   );
 }
