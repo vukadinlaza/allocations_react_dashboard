61e667366a6fbabab88c96fea0464a9c98b4b30f
Danny Suarez <suarezd10@gmail.com>
2021-06-15 10:31:12 -0700

    refactor with container components

--- src/components/Profile/addEntityModal.js
+++ src/components/Profile/addEntityModal.js
@@ -1,26 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { camelCase, pick } from 'lodash';
-import { gql } from 'apollo-boost';
+import { pick } from 'lodash';
 import CloseIcon from '@material-ui/icons/Close';
-import { useMutation } from '@apollo/react-hooks';
-
-import {
-  Paper,
-  Grid,
-  Typography,
-  Modal,
-  TextField,
-  Button,
-  MenuItem,
-  Select,
-  FormControl,
-  InputLabel,
-} from '@material-ui/core';
-import countries from 'country-region-data';
-import { UsaStates } from 'usa-states';
+import { Paper, Grid, Modal, Container } from '@material-ui/core';
 import { toast } from 'react-toastify';
-import { nWithCommas } from '../../utils/numbers';
 import EntityEditForm from './EntityEdit/index';
 
 import './style.scss';
@@ -36,8 +19,10 @@ const useStyles = makeStyles((theme) => ({
     marginTop: '8vh',
     borderRadius: '1rem',
     padding: theme.spacing(2),
-    maxHeight: '70%',
-    overflow: 'scroll',
+    [theme.breakpoints.down('sm')]: {
+      maxHeight: '70%',
+      overflow: 'scroll',
+    },
   },
   header: {
     fontSize: '1.5rem',
@@ -49,18 +34,6 @@ const useStyles = makeStyles((theme) => ({
     minWidth: '100%',
   },
 }));
-
-const entityInfo = {
-  'Dropdown 641a3b0c-3d28-4670-8a6e-d22eddc230e9': 'entity type',
-  MemberName: 'Name of Entity or individual',
-  SubAmount: 'subscription amount',
-  CountryResIndividual: 'Country of residence if individual',
-  PlaceBusinessEntity: 'Place Of business if Entity',
-  AccreditedIndividual: 'Net worth individual',
-  AccreditedEntity: 'Net Worth Entity',
-  'Email 23b66ce4-6d37-43f8-b44d-733d3d5b292f': 'user email',
-  'Name 4a0d1cec-823e-4580-94a0-75605f445380': 'User full name',
-};
 
 const INITIAL_STATE = {
   _id: '',
@@ -129,37 +102,39 @@ export default ({
   return (
     <>
       <Modal
-        open={showEntityModal}
+        open={Boolean(showEntityModal)}
         onClose={() => {}}
         className={classes.modal}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
-        <Grid container xs={12} sm={12} md={4} lg={5}>
-          <Grid item xs={12} sm={12} md={12} lg={12}>
-            <Paper className={classes.modalPaper}>
-              <Grid style={{ display: 'flex', justifyContent: 'flex-end', cursor: 'pointer' }}>
-                <CloseIcon
-                  onClick={() => {
-                    setEntityData({ ...INITIAL_STATE, accountId });
-                    setShowEntityModal(false);
-                  }}
+        <Container maxWidth="sm">
+          <Grid container>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              <Paper className={classes.modalPaper}>
+                <Grid style={{ display: 'flex', justifyContent: 'flex-end', cursor: 'pointer' }}>
+                  <CloseIcon
+                    onClick={() => {
+                      setEntityData({ ...INITIAL_STATE, accountId });
+                      setShowEntityModal(false);
+                    }}
+                  />
+                </Grid>
+                <EntityEditForm
+                  investor={entityData}
+                  icon={icon}
+                  setInvestor={setEntityData}
+                  setFormStatus={setFormStatus}
+                  actionText="Save Entity"
+                  submitfn={submit}
+                  deleteEntity={deleteEntity}
+                  isEdit={isEdit}
+                  handleClose={handleClose}
                 />
-              </Grid>
-              <EntityEditForm
-                investor={entityData}
-                icon={icon}
-                setInvestor={setEntityData}
-                setFormStatus={setFormStatus}
-                actionText="Save Entity"
-                submitfn={submit}
-                deleteEntity={deleteEntity}
-                isEdit={isEdit}
-                handleClose={handleClose}
-              />
-            </Paper>
+              </Paper>
+            </Grid>
           </Grid>
-        </Grid>
+        </Container>
       </Modal>
     </>
   );
