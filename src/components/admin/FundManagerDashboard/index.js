e0b623f7f2a4a91e02b80434e1bc8b99c8dce6fc
Adrian <adri.suscripciones@gmail.com>
2021-07-13 13:03:25 -0400

    minor fix

--- src/components/admin/FundManagerDashboard/index.js
+++ src/components/admin/FundManagerDashboard/index.js
@@ -437,7 +437,7 @@ const FundManagerDashboard = ({ classes, history }) => {
   const [openTooltip, setOpenTooltip] = useState('');
   const [getInvestments, { data: dealInvestments }] = useLazyQuery(GET_INVESTMENTS);
   const [getOrgDeals, { data: orgDeals }] = useLazyQuery(ORG_OVERVIEW);
-  const { data: subsData } = useSubscription(ONBOARDING, { variables: { data: "hello" } })
+  const { data: subsData } = useSubscription(ONBOARDING)
   const { data: atDeal } = useFetch(
     OPS_ACCOUNTING, 
     dealName && DEALS_TABLE, 
