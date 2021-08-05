61a6b89729428c88cb1911cfa08c60ad3bb4643a
Danny Suarez <DannySuarez@users.noreply.github.com>
2021-08-05 10:20:19 -0700

    refactor/investment page (part1) (#574)
    * refactor/investment page

--- src/components/DealOneClick/InvestmentPage/InvestmentAmount/index.js
+++ src/components/DealOneClick/InvestmentPage/InvestmentAmount/index.js
@@ -1,46 +1,56 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
-import './styles.scss';
+import { Grid, Button } from '@material-ui/core';
 import CurrencyTextField from '@unicef/material-ui-currency-textfield';
+import { PanelContainer, PanelLabel } from '../../../Panel';
+import useStyles, { Adornment } from './styles';
 import { nWithCommas } from '../../../../utils/numbers';
 
-function InvestmentAmountPanel({ setAmount, amount, minimumInvestment }) {
+const InvestmentAmountPanel = ({ setAmount, amount, minimumInvestment }) => {
+  const classes = useStyles();
   const placeHolder =
     minimumInvestment !== null && minimumInvestment ? nWithCommas(minimumInvestment) : '1,000';
-
   return (
-    <section className="InvestmentAmountPanel">
-      <p className="section-label">Investment Amount</p>
-      <div className="investment-amount-container">
-        <CurrencyTextField
-          className="investment-amount-input"
-          variant="outlined"
-          value={amount}
-          placeholder={placeHolder}
-          currencySymbol="$"
-          textAlign="left"
-          outputFormat="string"
-          decimalCharacter="."
-          decimalPlaces={0}
-          digitGroupSeparator=","
-          onKeyDown={(e) => {
-            if (e.key === 'Backspace' && amount === minimumInvestment) setAmount('');
-          }}
-          onChange={(event, value) => setAmount(value.toString())}
-        />
+    <PanelContainer>
+      <PanelLabel label="Investment Amount" />
+      <Grid container justifyContent="space-between" className={classes.root}>
+        <Grid item className={classes.inputContainer} xs={12} md={7} lg={7}>
+          <CurrencyTextField
+            InputProps={{
+              startAdornment: <Adornment position="start">$</Adornment>,
+            }}
+            style={{ width: '100%' }}
+            variant="outlined"
+            value={amount}
+            placeholder={placeHolder}
+            currencySymbol="$"
+            textAlign="left"
+            outputFormat="string"
+            decimalCharacter="."
+            decimalPlaces={0}
+            digitGroupSeparator=","
+            onKeyDown={(e) => {
+              if (e.key === 'Backspace' && amount === minimumInvestment) setAmount('');
+            }}
+            onChange={(value) => setAmount(value.toString())}
+          />
+        </Grid>
 
-        <Button
-          onClick={() =>
-            setAmount(minimumInvestment !== null && minimumInvestment ? minimumInvestment : '1000')
-          }
-          name="min-investment"
-          className="min-investment-button"
-        >
-          Minimum investment
-        </Button>
-      </div>
-    </section>
+        <Grid item xs={12} md={4} lg={4} className={classes.buttonContainer}>
+          <Button
+            className={classes.currencyButton}
+            onClick={() =>
+              setAmount(
+                minimumInvestment !== null && minimumInvestment ? minimumInvestment : '1000',
+              )
+            }
+            name="min-investment"
+          >
+            Minimum investment
+          </Button>
+        </Grid>
+      </Grid>
+    </PanelContainer>
   );
-}
+};
 
 export default InvestmentAmountPanel;
