e041744eabeca4b07f6a4afcafcd7abcaaaaa02f
Wes Griffin <wesgriffincodes@gmail.com>
2021-09-07 15:03:25 -0700

    added some styling to entities

--- src/components/Profile/sections/ProfileEntities.js
+++ src/components/Profile/sections/ProfileEntities.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import {
-  Typography,
   Paper,
   Grid,
   Button,
@@ -9,9 +8,12 @@ import {
   TableHead,
   TableCell,
   TableRow,
+  Fab,
 } from '@material-ui/core';
 import { capitalize } from 'lodash';
 import SettingsIcon from '@material-ui/icons/Settings';
+import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
+import EditIcon from '@material-ui/icons/Edit';
 import AddEntityModal from '../addEntityModal';
 
 const ProfileEntities = ({
@@ -35,52 +37,65 @@ const ProfileEntities = ({
   if (!accountEntities?.getEntities) return null;
 
   return (
-    <div>
-      <Paper className="account-paper">
-        <Grid style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography variant="h6" gutterBottom>
-            Entities
-          </Typography>
-          <Button color="primary" variant="contained" onClick={() => setShowEntityModal(true)}>
+    <Grid container spacing={2}>
+      <Grid item xs={12} container justifyContent="flex-end">
+        <Grid item>
+          <Button
+            color="primary"
+            variant="contained"
+            startIcon={<AddCircleRoundedIcon />}
+            onClick={() => setShowEntityModal(true)}
+            style={{ width: '200px' }}
+          >
             Add new
           </Button>
         </Grid>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Type</TableCell>
-              <TableCell align="center">Name </TableCell>
-              <TableCell align="center">Email</TableCell>
-              <TableCell align="center">Country</TableCell>
-              <TableCell align="center" />
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {accountEntities?.getEntities.map((row) => (
-              <TableRow key={row?._id}>
-                <TableCell component="th" scope="row">
-                  {capitalize(row.investor_type)}
-                </TableCell>
-                <TableCell align="center">{getName(row)}</TableCell>
-                <TableCell align="center">{row.email}</TableCell>
-                <TableCell align="center">{row.country}</TableCell>
-                <TableCell align="center">
-                  <SettingsIcon
-                    color={row?.isPrimaryEntity ? 'disabled' : 'primary'}
-                    style={{ cursor: row?.isPrimaryEntity ? 'not-allowed' : 'pointer' }}
-                    onClick={() => {
-                      if (row.isPrimaryEntity) {
-                        return;
-                      }
-                      setShowEntityModal(row);
-                    }}
-                  />
-                </TableCell>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Paper className="account-paper">
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Type</TableCell>
+                <TableCell align="center">Name </TableCell>
+                <TableCell align="center">Email</TableCell>
+                <TableCell align="center">Country</TableCell>
+                <TableCell align="center">Edit</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
+            </TableHead>
+
+            <TableBody>
+              {accountEntities?.getEntities.map((row) => (
+                <TableRow key={row?._id}>
+                  <TableCell component="th" scope="row">
+                    {capitalize(row.investor_type)}
+                  </TableCell>
+                  <TableCell align="center">{getName(row)}</TableCell>
+                  <TableCell align="center">{row.email}</TableCell>
+                  <TableCell align="center">{row.country}</TableCell>
+                  <TableCell align="center">
+                    <Fab
+                      size="small"
+                      color={row?.isPrimaryEntity ? 'disabled' : 'primary'}
+                      style={{ cursor: row?.isPrimaryEntity ? 'not-allowed' : 'pointer' }}
+                      onClick={() => {
+                        if (row.isPrimaryEntity) {
+                          return;
+                        }
+                        setShowEntityModal(row);
+                      }}
+                      aria-label="add"
+                    >
+                      <EditIcon />
+                    </Fab>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      </Grid>
 
       <AddEntityModal
         showEntityModal={showEntityModal}
@@ -91,7 +106,7 @@ const ProfileEntities = ({
         updateEntity={updateEntity}
         deleteEntity={deleteEntity}
       />
-    </div>
+    </Grid>
   );
 };
 
