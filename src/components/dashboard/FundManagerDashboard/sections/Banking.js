5fce83b241c599aece89cccbea59ec9728f19939
Cameron Zimmerman <cameronzimmerman@Camerons-MacBook-Pro.local>
2021-10-21 11:16:50 -0700

    table with dummy data

--- src/components/dashboard/FundManagerDashboard/sections/Banking.js
+++ src/components/dashboard/FundManagerDashboard/sections/Banking.js
@@ -2,12 +2,39 @@ import React, { useState } from 'react';
 import AllocationsTable from '../../../utils/AllocationsTable';
 import Loader from '../../../utils/Loader';
 
-const dummyData = {};
+const dummyData = Array.apply(' ', Array(100)).map((_, i) => {
+  return {
+    number: '00000000',
+    available: Math.random() >= 0.5 ? true : false,
+  };
+});
 
-const headers = {};
+const getCellContent = (type, row, headerValue) => {
+  switch (type) {
+    case 'number':
+      return row.number;
+    case 'available':
+      return row.available.toString();
+    default:
+      return <div />;
+  }
+};
+
+const headers = [
+  { value: 'number', label: 'REFERENCE NUMBER', align: 'left', alignHeader: true, type: 'number' },
+  { value: 'available', label: 'AVAILABLE', type: 'available', align: 'left', alignHeader: true },
+];
 
 const Banking = ({ deal_id }) => {
-  return <h1>Banking</h1>;
+  return (
+    <AllocationsTable
+      data={dummyData}
+      headers={headers}
+      getCellContent={getCellContent}
+      sortField="available"
+      sortOrder="desc"
+    />
+  );
 };
 
 export default Banking;
