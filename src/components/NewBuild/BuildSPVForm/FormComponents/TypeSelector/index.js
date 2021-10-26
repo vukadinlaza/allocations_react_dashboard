2928b552fb382f6a1c62ad4256854176e4f9abe4
Bryana Kitchen <bryanakitchen@gmail.com>
2021-10-26 17:29:01 -0400

    merge conflicts

--- src/components/NewBuild/BuildSPVForm/FormComponents/TypeSelector/index.js
+++ src/components/NewBuild/BuildSPVForm/FormComponents/TypeSelector/index.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 
 import HelpIcon from '@material-ui/icons/Help';
-import { TextField, Paper, Grid, FormControl, Button } from '@material-ui/core';
+import { TextField, Paper, Grid, FormControl, MenuItem } from '@material-ui/core';
 import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import Typography from '@material-ui/core/Typography';
+import { toast } from 'react-toastify';
 import useStyles from '../../../BuildStyles';
 import TypeItem from './TypeItem/index';
 import RocketIcon from '../../../../../assets/buildRocket.svg';
@@ -17,7 +18,6 @@ import NetworkIcon from '../../../../../assets/buildNetwork.svg';
 import PieIcon from '../../../../../assets/buildPie.svg';
 import { ModalTooltip } from '../../../../dashboard/FundManagerDashboard/widgets';
 import sectors from './sectors';
-import { toast } from 'react-toastify';
 
 export default function TypeSelector({
   assetType,
@@ -161,6 +161,9 @@ export default function TypeSelector({
       </>
     );
   }
+
+  const securities = ['one', 'two', 'three'];
+
   return (
     <Paper className={classes.paper}>
       <form noValidate autoComplete="off" className={classes.formContainers}>
@@ -188,6 +191,8 @@ export default function TypeSelector({
           <FormRow rowItems={row1Items} />
           <FormRow rowItems={row2Items} />{' '}
         </Grid>
+
+        {/* FIRST ROW */}
         <Grid container spacing={1} className={classes.inputGridContainer}>
           <Grid className={classes.inputGridItem} item xs={6}>
             <FormControl required disabled variant="outlined" className={classes.formContainers}>
@@ -224,62 +229,186 @@ export default function TypeSelector({
           <Grid className={classes.inputGridItem} item xs={6}>
             <FormControl required disabled variant="outlined" className={classes.formContainers}>
               <Typography className={classes.formItemName}>
-                Manager Name
+                Portfolio company securities?
                 <ModalTooltip
-                  title="Manager Name"
-                  handleTooltip={handleTooltip}
-                  tooltipContent={
-                    <Typography color="inherit">Full name of the manager of your SPV</Typography>
-                  }
-                  openTooltip={openTooltip}
-                  id="manager_name"
-                >
-                  <HelpIcon
-                    className={classes.helpIcon}
-                    onClick={(e) => handleTooltip('manager_name')}
-                  />
-                </ModalTooltip>
-              </Typography>
-              <TextField
-                value={buildData.manager_name}
-                name="manager_name"
-                onChange={handleChange}
-                className={classes.inputBox}
-                variant="outlined"
-              />
-            </FormControl>
-          </Grid>
-          <Grid className={classes.inputGridItem} item xs={6}>
-            <FormControl required disabled variant="outlined" className={classes.formContainers}>
-              <Typography className={classes.formItemName}>
-                Closing Date
-                <ModalTooltip
-                  title="Closing Date"
+                  title="Company Securities"
                   handleTooltip={handleTooltip}
                   tooltipContent={
                     <Typography color="inherit">
-                      Date on when the SPV needs to make the money transfer
+                      Indicate what kind of security the SPV will acquire (e.g., Series A Preferred
+                      Stock, Simple Agreement for Future Equity, Convertible Promissory Note or
+                      other)
                     </Typography>
                   }
                   openTooltip={openTooltip}
-                  id="closing_date"
+                  // IS THIS ACCURATE?
+                  id="portfolio_company_securities"
                 >
                   <HelpIcon
                     className={classes.helpIcon}
-                    onClick={(e) => handleTooltip('closing_date')}
+                    onClick={(e) => handleTooltip('portfolio_company_securities')}
                   />
                 </ModalTooltip>
               </Typography>
-              <TextField
-                value={buildData.closing_date}
-                name="closing_date"
+              <Select
+                id="portfolio_company_securities"
+                value={buildData.portfolio_company_securities}
+                label="Portfolio Company Securities"
+                value={securities.map((security) => ({ value: security, label: security })) || ''}
+                onChange={(option) => {
+                  const newEvent = {
+                    target: {
+                      name: 'portfolio_company_securities',
+                      value: option.map((security) => security.value),
+                    },
+                  };
+                  handleChange(newEvent);
+                }}
+              >
+                <MenuItem value="one">One</MenuItem>
+                <MenuItem value="two">Two</MenuItem>
+              </Select>
+              {/* <TextField
+                value={buildData.portfolio_company_securities}
+                name="portfolio_company_securities"
                 onChange={handleChange}
                 className={classes.inputBox}
                 variant="outlined"
-                type="date"
-              />
+                placeholder="SpaceX"
+              /> */}
             </FormControl>
           </Grid>
+
+          {/* SECOND ROW */}
+          <Grid container spacing={1} className={classes.inputGridContainer}>
+            <Grid className={classes.inputGridItem} item xs={6}>
+              <FormControl required disabled variant="outlined" className={classes.formContainers}>
+                <Typography className={classes.formItemName}>
+                  Deal Name
+                  <ModalTooltip
+                    title="Deal Name"
+                    handleTooltip={handleTooltip}
+                    tooltipContent={
+                      <Typography color="inherit">
+                        A name to identify your deal (name of your series SPV in case you are a HVP)
+                      </Typography>
+                    }
+                    openTooltip={openTooltip}
+                    id="portfolio_deal_name"
+                  >
+                    <HelpIcon
+                      className={classes.helpIcon}
+                      onClick={(e) => handleTooltip('portfolio_deal_name')}
+                    />
+                  </ModalTooltip>
+                </Typography>
+                <TextField
+                  value={buildData.portfolio_deal_name}
+                  name="portfolio_deal_name"
+                  onChange={handleChange}
+                  className={classes.inputBox}
+                  variant="outlined"
+                  placeholder="e.x. crypto deal"
+                />
+              </FormControl>
+            </Grid>
+            <Grid className={classes.inputGridItem} item xs={6}>
+              <FormControl required disabled variant="outlined" className={classes.formContainers}>
+                <Typography className={classes.formItemName}>
+                  Closing Date
+                  <ModalTooltip
+                    title="Closing Date"
+                    handleTooltip={handleTooltip}
+                    tooltipContent={
+                      <Typography color="inherit">
+                        Date on when the SPV needs to make the money transfer
+                      </Typography>
+                    }
+                    openTooltip={openTooltip}
+                    id="closing_date"
+                  >
+                    <HelpIcon
+                      className={classes.helpIcon}
+                      onClick={(e) => handleTooltip('closing_date')}
+                    />
+                  </ModalTooltip>
+                </Typography>
+                <TextField
+                  value={buildData.closing_date}
+                  name="closing_date"
+                  onChange={handleChange}
+                  className={classes.inputBox}
+                  variant="outlined"
+                  type="date"
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          {/* THIRD ROW */}
+          <Grid container spacing={1} className={classes.inputGridContainer}>
+            <Grid className={classes.inputGridItem} item xs={6}>
+              <FormControl required disabled variant="outlined" className={classes.formContainers}>
+                <Typography className={classes.formItemName}>
+                  Manager Name
+                  <ModalTooltip
+                    title="Manager Name"
+                    handleTooltip={handleTooltip}
+                    tooltipContent={
+                      <Typography color="inherit">Full name of the manager of your SPV</Typography>
+                    }
+                    openTooltip={openTooltip}
+                    id="manager_name"
+                  >
+                    <HelpIcon
+                      className={classes.helpIcon}
+                      onClick={(e) => handleTooltip('manager_name')}
+                    />
+                  </ModalTooltip>
+                </Typography>
+                <TextField
+                  value={buildData.manager_name}
+                  name="manager_name"
+                  onChange={handleChange}
+                  className={classes.inputBox}
+                  variant="outlined"
+                />
+              </FormControl>
+            </Grid>
+            <Grid className={classes.inputGridItem} item xs={6}>
+              <FormControl required disabled variant="outlined" className={classes.formContainers}>
+                <Typography className={classes.formItemName}>
+                  Representative of the manager and its title?
+                  <ModalTooltip
+                    title="Representative"
+                    handleTooltip={handleTooltip}
+                    tooltipContent={
+                      <Typography color="inherit">
+                        Please indicate the name of the representative of the Manager as well as the
+                        title of the said person; applicable only if the Manager is a legal entity
+                      </Typography>
+                    }
+                    openTooltip={openTooltip}
+                    id="representative"
+                  >
+                    <HelpIcon
+                      className={classes.helpIcon}
+                      onClick={(e) => handleTooltip('representative')}
+                    />
+                  </ModalTooltip>
+                </Typography>
+                <TextField
+                  value={buildData.representative}
+                  name="representative"
+                  onChange={handleChange}
+                  className={classes.inputBox}
+                  variant="outlined"
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          {/* FOURTH ROW */}
           <Grid className={classes.inputGridItem} item xs={12}>
             <Typography className={classes.formItemName}>
               Sector(s) <HelpIcon className={classes.helpIcon} />
