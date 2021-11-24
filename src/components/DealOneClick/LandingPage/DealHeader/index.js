0066152bc77fba0167d3e57f28437eaed2ef5549
cmazariegos44 <claudias.mazariegos@gmail.com>
2021-11-23 16:39:45 -0800

    added progress-line in deal header component

--- src/components/DealOneClick/LandingPage/DealHeader/index.js
+++ src/components/DealOneClick/LandingPage/DealHeader/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { Paper, Avatar, Button, Box } from '@material-ui/core';
-import { useStyles } from './style';
+import React, { useState, useEffect } from 'react';
+import { Paper, Avatar, Button, Box, CardMedia, Grid, LinearProgress } from '@material-ui/core';
 import { HiOutlinePencil } from 'react-icons/hi';
+import { useStyles } from './style';
 
 function DealHeader({ deal }) {
-  const { company_name } = deal;
+  const { company_name, dealCoverImageKey, slug } = deal;
   const classes = useStyles();
+
+  const key = dealCoverImageKey?.includes('https')
+    ? dealCoverImageKey
+    : `https://allocations-public.s3.us-east-2.amazonaws.com/${dealCoverImageKey}`;
+
+  const [img, setImg] = useState(key);
+
+  useEffect(() => {
+    setImg(key);
+  }, [dealCoverImageKey, slug, key]);
 
   return (
     <Paper className={classes.dealHeader}>
@@ -18,6 +28,26 @@ function DealHeader({ deal }) {
           Edit
         </Button>
       </Box>
+
+      <Grid container>
+        <Grid item xs={6}>
+          <CardMedia
+            component="img"
+            height="140"
+            width="140"
+            alt="SPV Header Image"
+            src={img}
+            onError={() =>
+              setImg('https://allocations-public.s3.us-east-2.amazonaws.com/deals/default.png')
+            }
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <h3>Investment Progress</h3>
+          <h6 style={{ color: '#0561ff' }}>$320,000</h6>
+          <LinearProgress variant="determinate" value="10" />
+        </Grid>
+      </Grid>
     </Paper>
   );
 }
--- src/components/DealOneClick/LandingPage/DealHeader/style.js
+++ src/components/DealOneClick/LandingPage/DealHeader/style.js
@@ -1,8 +1,21 @@
 import { makeStyles } from '@material-ui/core';
 
+// eslint-disable-next-line import/prefer-default-export
 export const useStyles = makeStyles({
   dealHeader: {
     width: '100%',
     padding: '58px',
+  },
+
+  imageWrapper: {
+    justifySelf: 'flex-end',
+    width: '50%',
+    height: '50%',
+    margin: '0',
+    padding: '0',
+    overflow: 'hidden',
+    // objectFit: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
   },
 });
