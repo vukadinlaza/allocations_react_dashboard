5f910a551eae3b4d28e78169eccff9d91f967be2
Chase Abbott <cabbott93@gmail.com>
2021-12-20 09:54:42 -0800

    deal type

--- src/components/NewBuild/DealType.js
+++ src/components/NewBuild/DealType.js
@@ -482,12 +482,22 @@ export const NewBuildFinalWarning = ({ isOpen, closeModal, setPage }) => {
   );
 };
 
-export default function DealTypeSelector({ closeModal, isOpen, setDealType, setPage }) {
+export default function DealTypeSelector({
+  closeModal,
+  isOpen,
+  setDealType,
+  setPage,
+  next = {
+    spv: () => setPage('select_org'),
+    fund: () => setPage('select_org'),
+  },
+  onClose = () => closeModal(),
+}) {
   const classes = useStyles();
   const { width } = useViewport();
 
   return (
-    <Modal open={isOpen} onClose={closeModal} className={classes.modal}>
+    <Modal open={isOpen} onClose={onClose} className={classes.modal}>
       <Container style={{ width: '650px' }}>
         <Grid container style={{ height: '100%' }}>
           <Grid item xs={12} sm={12} md={12} lg={12} style={{ height: '100%' }}>
@@ -496,7 +506,7 @@ export default function DealTypeSelector({ closeModal, isOpen, setDealType, setP
                 <Typography variant="h6" style={{ color: '#fff' }}>
                   Add New
                 </Typography>
-                <Box onClick={closeModal} style={{ cursor: 'pointer' }}>
+                <Box onClick={onClose} style={{ cursor: 'pointer' }}>
                   <CloseIcon htmlColor="#fff" />
                 </Box>
               </Grid>
@@ -555,7 +565,7 @@ export default function DealTypeSelector({ closeModal, isOpen, setDealType, setP
                             }}
                             onClick={() => {
                               setDealType('spv');
-                              setPage('select_org');
+                              next.spv();
                             }}
                           >
                             Continue
@@ -598,7 +608,7 @@ export default function DealTypeSelector({ closeModal, isOpen, setDealType, setP
                             }}
                             onClick={() => {
                               setDealType('fund');
-                              setPage('select_org');
+                              next.fund();
                             }}
                           >
                             Continue
