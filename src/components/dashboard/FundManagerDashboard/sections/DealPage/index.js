3f794d792a4491ab62bbfe7a31f6ebca9452cd5d
Drew Radcliff <sirampr@gmail.com>
2022-01-04 14:48:05 -0500

    Fix deal page

--- src/components/dashboard/FundManagerDashboard/sections/DealPage/index.js
+++ src/components/dashboard/FundManagerDashboard/sections/DealPage/index.js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useFlags } from 'launchdarkly-react-client-sdk';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import EditIcon from '@material-ui/icons/Edit';
-import { Tooltip, Typography, Box, Container } from '@material-ui/core';
+import { Tooltip, Typography } from '@material-ui/core';
 import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
-import { CreateOutlined, VisibilityOutlined } from '@material-ui/icons';
 import { FlatBox } from '../../widgets';
 import DealLandingPageRedesign from '../../../../DealOneClick/LandingPageRedesign';
-import DealButton from '../../../../DealOneClick/LandingPageRedesign/DealButton';
 
 export default function DealPage({
   orgSlug,
@@ -18,41 +16,15 @@ export default function DealPage({
   handleLinkCopy,
 }) {
   const { dealPageRedesign } = useFlags();
-  const [isEdit, setIsEdit] = useState(false);
 
   if (dealPageRedesign)
     return (
-      <>
-        <Container maxWidth="md">
-          <Box display="flex" justifyContent="flex-end">
-            {!isEdit ? (
-              <DealButton
-                secondary
-                onClick={() => setIsEdit(true)}
-                icon={<CreateOutlined />}
-                text="Edit"
-                style={{ marginRight: '8px' }}
-              />
-            ) : (
-              <DealButton
-                onClick={() => setIsEdit(false)}
-                secondary
-                text="Discard Changes"
-                style={{ marginRight: '8px' }}
-              />
-            )}
-            <DealButton
-              secondary
-              onClick={goToDeal}
-              icon={<VisibilityOutlined />}
-              text="Preview"
-              style={{ marginRight: '8px' }}
-            />
-            {isEdit && <DealButton text="Save" />}
-          </Box>
-        </Container>
-        <DealLandingPageRedesign orgSlug={orgSlug} dealSlug={dealData.slug} isEdit={isEdit} />
-      </>
+      <DealLandingPageRedesign
+        orgSlug={orgSlug}
+        dealSlug={dealData.slug}
+        goToDeal={goToDeal}
+        dealPageRedesign={dealPageRedesign}
+      />
     );
 
   return (
