14d72ffe4c81fe5e7220af4a752bd407a74eabb0
Aaron Dennis <amdennis1987@gmail.com>
2022-02-11 14:45:10 -0800

    Invest journey

--- cypress/integration/investingJourney/investing.js
+++ cypress/integration/investingJourney/investing.js
@@ -77,9 +77,106 @@ describe('Investing Journey', () => {
     cy.findByRole('heading', { name: /\$5,000/i }).contains(/^\$\d{1,3}(,\d{3})*(\.\d+)?$/);
     cy.findByRole('button', { name: /invest/i }).click();
     cy.url().should('eq', 'http://localhost:3000/invest/allocations/demo-space-x');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000);
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(1) > div:nth-child(2)',
+    )
+      .find('input')
+      .click()
+      .clear()
+      .type('5000')
+      .should('have.value', '5,000')
+      .clear();
+    cy.findByRole('button', { name: /minimum investment/i }).click();
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(1) > div:nth-child(2)',
+    )
+      .find('input')
+      .should('have.value', '5,000.00')
+      .clear();
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > div > section',
+    ).contains('Deal documents');
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(4) > div',
+    )
+      .contains('Terms and Conditions')
+      .and('be.visible');
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(4) > div > div',
+    ).contains(
+      'I consent to electronic delivery of all documents, notices and agreements as related to my investment;',
+    );
+    cy.findByRole('button', { name: /back to deal page/i }).click();
+    cy.url().should('include', '/deals/allocations/demo-space-x');
+    cy.findByRole('button', { name: /invest/i }).click();
+    cy.findByRole('button', { name: /next steps/i }).click();
+    cy.url().should('include', '/next-steps/allocations/demo-space-x');
+    cy.findByRole('button', { name: /edit investment/i }).click();
+    cy.url().should('include', '/invest/allocations/demo-space-x');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000);
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(3) > div:nth-child(3)',
+    )
+      .find('input')
+      .click()
+      .clear()
+      .type('Entity Name')
+      .should('have.value', 'Entity Name');
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(3) > div:nth-child(2)',
+    ).click();
+    cy.findByRole('option', { name: /individual/i }).click();
+    cy.findByRole('textbox', { name: /country/i }).click();
+    cy.findByRole('option', { name: /^United States$/i }).click();
+    cy.findByRole('textbox', { name: /state/i }).click();
+    cy.findByRole('option', { name: /^Alabama$/i }).click();
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(3) > div:nth-child(6)',
+    ).click();
+    cy.findByRole('option', {
+      name: /My professional certification qualifies me as an accredited investor/,
+    }).click();
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(4)',
+    )
+      .find('input')
+      .click();
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(4) > div:nth-child(2) > div > form > div:nth-child(1)',
+    )
+      .find('input')
+      .click()
+      .clear()
+      .type('Second Signer')
+      .should('have.value', 'Second Signer');
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(4) > div:nth-child(2) > div > form > div:nth-child(2)',
+    )
+      .find('input')
+      .click()
+      .clear()
+      .type('second@email.com')
+      .should('have.value', 'second@email.com');
+    cy.get(
+      '#root > div:nth-child(2) > div:nth-child(2) > section > div:nth-child(3) > section:nth-child(4) > div:nth-child(2) > div > div',
+    )
+      .find('input')
+      .click();
+    cy.findByRole('checkbox', {
+      name: /we have read and accept the terms of the investment\./i,
+    }).click();
+    cy.findByRole('button', { name: /confirm and submit for final agreement review/i }).click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(4000);
+    cy.findByTitle(/spv document/i).should('be.visible');
+    cy.findByRole('button', { name: /we agree/i }).should('be.visible');
+    cy.findByRole('button', { name: /we decline/i }).click();
   });
-  it('logs out', () => {
-    cy.findByText(/logout/i).click();
-    cy.url().should('include', 'https://staging.login.allocations.com/');
-  });
+  // it('logs out', () => {
+  //   cy.findByText(/logout/i).click();
+  //   cy.url().should('include', 'https://staging.login.allocations.com/');
+  // });
 });
