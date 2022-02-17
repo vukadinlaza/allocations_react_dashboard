dfa11663fc15ee8269aa9efd68aaf71b526ec421
Chase Abbott <cabbott93@gmail.com>
2022-02-16 16:07:29 -0800

    add package to stop caching build

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const ExternalTemplateRemotesPlugin = require('external-remotes-plugin');
 
 module.exports = {
   webpack: {
@@ -10,7 +11,7 @@ module.exports = {
         filename: 'remoteEntry.js',
         remotes: {
           invest: `invest@https://invest-frontend-staging.herokuapp.com/_next/static/chunks/remoteEntry.js`,
-          build: `build@${process.env.BUILD_FRONTEND_URL}/_next/static/chunks/remoteEntry.js`,
+          build: `build@[window.buildURL]/_next/static/chunks/remoteEntry.js`,
           treasury: `treasury@${process.env.TREASURY_FRONTEND_URL}/_next/static/chunks/remoteEntry.js`,
           blockchain: `blockchain@${process.env.CRYPTO_FRONTEND_URL}/_next/static/chunks/remoteEntry.js`,
         },
@@ -37,6 +38,7 @@ module.exports = {
           },
         },
       }),
+      new ExternalTemplateRemotesPlugin(),
     ],
     configure: (webpackConfig) => {
       // webpackConfig.output.enabledLibraryTypes = ['var'];
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 // eslint-disable-next-line no-unused-expressions
+
+window.buildURL = process.env.REACT_APP_BUILD_FRONTEND_URL;
+
 import('./bootstrap');
