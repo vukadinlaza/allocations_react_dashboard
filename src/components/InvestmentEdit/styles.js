651e2ed1acb596abde333f87ccc1f0baa6cc0beb
Bryana Kitchen <bryanakitchen@gmail.com>
2022-04-05 08:15:06 -0400

    remove unused var

--- src/components/InvestmentEdit/styles.js
+++ src/components/InvestmentEdit/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const styles = makeStyles((theme) => ({
+const styles = makeStyles(() => ({
   addDoc: {
     display: 'inline-block',
     height: '90px',
