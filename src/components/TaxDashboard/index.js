b8624fc6050c05ca5ea231949667a59c401a92b3
Ryan Mehta <ryan.mehta@gmail.com>
2022-04-08 09:47:57 -0700

    add management

--- src/components/TaxDashboard/index.js
+++ src/components/TaxDashboard/index.js
@@ -1,16 +1,59 @@
 import { useFlags } from 'launchdarkly-react-client-sdk';
-import React from 'react';
+import React, { useState } from 'react';
 import Loader from '../utils/Loader';
 
-const TaxDashboard = React.lazy(() => import('taxdashboard/TaxDashboard'));
-const InvestorDashboard = React.lazy(() => import('tax/InvestorDashboard'));
+const OldTaxDashboard = React.lazy(() => import('taxdashboard/TaxDashboard'));
+const TaxDashboard = React.lazy(() => import('tax/TaxDashboard'));
+const TaxReturnDashboard = React.lazy(() => import('tax/TaxReturnDashboard'));
+const InvestorView = React.lazy(() => import('tax/InvestorView'));
 
 export default function RemoteTaxDashboard() {
-  const { taxActivity } = useFlags();
+  const [manage, setManage] = useState(null);
+  const [viewInvestor, setViewInvestor] = useState(null);
+  const { taxActivity, taxActivityManage } = useFlags();
+
+  if (taxActivity === 'tax-dashboard') {
+    return (
+      <React.Suspense fallback={<Loader />}>
+        <OldTaxDashboard />
+      </React.Suspense>
+    );
+  }
+
+  if (taxActivityManage) {
+    if (viewInvestor) {
+      return (
+        <React.Suspense fallback={<Loader />}>
+          <InvestorView
+            email={viewInvestor}
+            onBackToDashboard={() => {
+              setManage(null);
+              setViewInvestor(null);
+            }}
+            onBackToReturn={() => setViewInvestor(null)}
+          />
+        </React.Suspense>
+      );
+    }
+
+    if (manage) {
+      return (
+        <React.Suspense fallback={<Loader />}>
+          <TaxReturnDashboard
+            id={manage}
+            onBack={() => setManage(null)}
+            onViewInvestor={setViewInvestor}
+          />
+        </React.Suspense>
+      );
+    }
+  }
 
   return (
     <React.Suspense fallback={<Loader />}>
-      {taxActivity === 'tax-dashboard' ? <TaxDashboard /> : <InvestorDashboard />}
+      <TaxDashboard
+        onManage={taxActivityManage ? (taxReturn) => setManage(taxReturn._id) : undefined}
+      />
     </React.Suspense>
   );
 }
