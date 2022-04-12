38cde27b0fcbc9a05b9ef1b95847048c6c27edb0
Ryan Mehta <ryan.mehta@gmail.com>
2022-04-11 18:07:07 -0700

    get back button working better

--- src/components/TaxDashboard/index.js
+++ src/components/TaxDashboard/index.js
@@ -1,5 +1,7 @@
 import { useFlags } from 'launchdarkly-react-client-sdk';
 import React, { useState } from 'react';
+import { useHistory, useLocation } from 'react-router';
+import { useCurrentOrganization } from '../../state/current-organization';
 import Loader from '../utils/Loader';
 
 const OldTaxDashboard = React.lazy(() => import('taxdashboard/TaxDashboard'));
@@ -8,9 +10,15 @@ const TaxReturnDashboard = React.lazy(() => import('tax/TaxReturnDashboard'));
 const InvestorView = React.lazy(() => import('tax/InvestorView'));
 
 export default function RemoteTaxDashboard() {
-  const [manage, setManage] = useState(null);
-  const [viewInvestor, setViewInvestor] = useState(null);
+  const history = useHistory();
+  const location = useLocation();
+  const currentOrganization = useCurrentOrganization();
+  const params = new URLSearchParams(location.search);
+  const [manage, setManage] = useState(params.get('tax_return') || null);
+  const [viewInvestor, setViewInvestor] = useState(params.get('email') || null);
   const { taxActivity, taxActivityManage } = useFlags();
+
+  if (!currentOrganization) return null;
 
   if (taxActivity === 'tax-dashboard') {
     return (
@@ -27,10 +35,14 @@ export default function RemoteTaxDashboard() {
           <InvestorView
             email={viewInvestor}
             onBackToDashboard={() => {
+              history.push('/tax-activity');
               setManage(null);
               setViewInvestor(null);
             }}
-            onBackToReturn={() => setViewInvestor(null)}
+            onBackToReturn={() => {
+              history.push(`?tax_activity=${manage}`);
+              setViewInvestor(null);
+            }}
           />
         </React.Suspense>
       );
@@ -41,18 +53,30 @@ export default function RemoteTaxDashboard() {
         <React.Suspense fallback={<Loader />}>
           <TaxReturnDashboard
             id={manage}
-            onBack={() => setManage(null)}
-            onViewInvestor={setViewInvestor}
+            onBack={() => {
+              history.push('/tax-activity');
+              setManage(null);
+            }}
+            onViewInvestor={(email) => {
+              history.push(`?tax_return=${manage}&email=${email}`);
+              setViewInvestor(email);
+            }}
           />
         </React.Suspense>
       );
     }
   }
 
+  const handleManage = (taxReturn) => {
+    history.push(`?tax_return=${taxReturn._id}`);
+    setManage(taxReturn._id);
+  };
+
   return (
     <React.Suspense fallback={<Loader />}>
       <TaxDashboard
-        onManage={taxActivityManage ? (taxReturn) => setManage(taxReturn._id) : undefined}
+        onManage={taxActivityManage ? handleManage : undefined}
+        defaultView={currentOrganization.name.includes('@') ? 'investor' : 'fund-manager'}
       />
     </React.Suspense>
   );
