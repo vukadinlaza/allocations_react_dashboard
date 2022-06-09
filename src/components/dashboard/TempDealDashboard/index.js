6dc295084389d4a35236efff762d28dee076f029
Ryan Mehta <ryan.mehta@gmail.com>
2022-06-09 08:18:31 -0700

    create dashboard deal on submit build

--- src/components/dashboard/TempDealDashboard/index.js
+++ src/components/dashboard/TempDealDashboard/index.js
@@ -7,7 +7,6 @@ import { Typography as AllocationsTypography } from '@allocations/design-system'
 import Setup from './sections/Setup';
 import Highlights from './sections/Highlights';
 import InvestorStatus from './sections/InvestorStatus';
-import Banking from './sections/Banking';
 import Crypto from './sections/Crypto';
 import Investments from './sections/Investments';
 import Investors from './sections/Investors';
@@ -21,6 +20,7 @@ import DealPage from '../Common/DealPage';
 import HighlightedTabs from '../../utils/HighlightedTabs';
 import Loader from '../../utils/Loader';
 import { phone } from '../../../utils/helpers';
+import PostBuild from '../../PostBuild';
 
 const RemoteInvestors = React.lazy(() => import('invest/Investors'));
 const ProgressBar = React.lazy(() => import('build/ProgressBar'));
@@ -110,7 +110,13 @@ let BASE = 'app3m4OJvAWUg0hng';
 const INVESTMENTS_TABLE = 'Investments';
 const DEALS_TABLE = 'Deals';
 
-let spvTabs = ['Investor Onboarding Status', 'Investors', 'Documents', 'Deal Page'];
+let spvTabs = [
+  'Deal Progress',
+  'Investor Onboarding Status',
+  'Investors',
+  'Documents',
+  'Deal Page',
+];
 
 const TempDealDashboard = () => {
   const { width } = useViewport();
@@ -149,15 +155,6 @@ const TempDealDashboard = () => {
   const checkedDealName = encodeURIComponent(dealName);
   const checkedAtDealDataName = encodeURIComponent(atDealData?.name);
 
-  if (userProfile.admin) {
-    const bankingTabName = 'Banking';
-    // Only add banking tab if user is admin and FF fundManagerBankingTab is true
-    if (fundManagerBankingTab) {
-      if (!fundTabs.includes(bankingTabName)) fundTabs.push(bankingTabName);
-      if (!spvTabs.includes(bankingTabName)) spvTabs.push(bankingTabName);
-    }
-  }
-
   if (userProfile.admin && cryptoPaymentInBuild) {
     // const cryptoTabName = 'Crypto';
     // // Only add crypto tab if user is admin
@@ -178,7 +175,13 @@ const TempDealDashboard = () => {
   );
 
   useEffect(() => {
-    spvTabs = ['Investor Onboarding Status', 'Investors', 'Documents', 'Deal Page'];
+    spvTabs = [
+      'Deal Progress',
+      'Investor Onboarding Status',
+      'Investors',
+      'Documents',
+      'Deal Page',
+    ];
     (async () => {
       try {
         if (remoteFundManagerDashboard) {
@@ -316,6 +319,9 @@ const TempDealDashboard = () => {
       case 'Investments':
         return <Investments classes={classes} data={fundData} />;
 
+      case 'Deal Progress':
+        return <PostBuild dealId={dealData?.deal._id} />;
+
       case 'Investor Onboarding Status':
         return (
           <InvestorStatus
@@ -353,15 +359,6 @@ const TempDealDashboard = () => {
             goToDeal={goToDeal}
             goToEditDeal={goToEditDeal}
             handleLinkCopy={handleLinkCopy}
-          />
-        );
-      case 'Banking':
-        return (
-          <Banking
-            dealData={remoteFundManagerDashboard ? serviceDeal : dealData}
-            deal_id={remoteFundManagerDashboard ? serviceDeal._id : dealData.deal._id}
-            virtual_account_number={dealData.deal.virtual_account_number || null}
-            classes={classes}
           />
         );
       case 'Crypto':
