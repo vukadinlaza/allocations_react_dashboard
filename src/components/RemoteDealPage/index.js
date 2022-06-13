589fc2c314aa182a8c9238bb7f287f25ced21966
Aaron Dennis <amdennis1987@gmail.com>
2022-06-13 13:58:19 -0700

    add userProfile to remote deal page

--- src/components/RemoteDealPage/index.js
+++ src/components/RemoteDealPage/index.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import { Redirect, useHistory, useLocation, useParams } from 'react-router';
+import { useAuth } from '../../auth/useAuth';
 import Loader from '../utils/Loader';
 
 const DealPage = React.lazy(() => import('build/DealPage'));
@@ -8,6 +9,7 @@ export default function RemoteDealPage() {
   const history = useHistory();
   const { pathname } = useLocation();
   const { organization, deal_id, deal_slug } = useParams();
+  const { userProfile } = useAuth();
   return (
     <Suspense fallback={<Loader />}>
       <DealPage
@@ -18,6 +20,7 @@ export default function RemoteDealPage() {
         pushToDealPage={() => history.push(`/admin/${organization}/${deal_id}`)}
         goToInvestPage={() => history.push(`/invest/${deal_id}`)}
         redirectTo404={() => <Redirect to="/404" />}
+        user={userProfile}
       />
     </Suspense>
   );
