0320d18f6e95a3e09c9cd3e307e049ab8b4a4492
Adrian Monasterios Gutierrez <adri.suscripciones@gmail.com>
2022-06-22 14:54:37 -0400

    master to staging merge trough other branch

--- src/components/RemoteFundManagerDashboard/index.js
+++ src/components/RemoteFundManagerDashboard/index.js
@@ -1,6 +1,5 @@
 import React, { Suspense } from 'react';
 import { Redirect, useHistory, useParams } from 'react-router';
-import { useAuth } from '../../auth/useAuth';
 import Loader from '../utils/Loader';
 
 const ManagerDashboard = React.lazy(() => import('build/FundManagerDashboard'));
@@ -8,12 +7,10 @@ const ManagerDashboard = React.lazy(() => import('build/FundManagerDashboard'));
 export default function RemoteFundManagerDashboard() {
   const history = useHistory();
   const { organization } = useParams();
-  const { userProfile } = useAuth();
 
   return (
     <Suspense fallback={<Loader />}>
       <ManagerDashboard
-        user={userProfile}
         orgSlug={organization}
         pushToDealPage={(deal_id) => history.push(`/admin/${organization}/deals/${deal_id}`)}
         pushToCreateNewDealPage={() => history.push(`/admin/${organization}/deal/new`)}
