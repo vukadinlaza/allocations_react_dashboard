5d11d5a11297efb021e1ef47fcaa236bde56e11f
Adrian Monasterios Gutierrez <adri.suscripciones@gmail.com>
2022-07-12 16:19:39 -0400

    remove unused query

--- src/api/deals.js
+++ src/api/deals.js
@@ -1,18 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const search = gql`
-  query SearchDeals($q: String!) {
-    searchDeals(q: $q) {
-      _id
-      company_name
-      company_description
-    }
-  }
-`;
-
-export const searchByOrg = gql`
-  query SearchDealsByOrg($q: String!, $org: String!) {
-    searchDealsByOrg(q: $q, org: $org) {
+  query DealsByField($field: String!, $searchTerm: String) {
+    dealsByField(field: $field, searchTerm: $searchTerm) {
       _id
       company_name
       company_description
