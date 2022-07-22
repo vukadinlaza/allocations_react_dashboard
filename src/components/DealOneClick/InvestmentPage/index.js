957636f991d88a60fe706fca3ce1ee3611e5fa67
Lance Merrill <lanceomerrill@gmail.com>
2022-07-22 05:46:02 -0700

    add validation

--- src/components/DealOneClick/InvestmentPage/index.js
+++ src/components/DealOneClick/InvestmentPage/index.js
@@ -257,7 +257,11 @@ function InvestmentPage() {
       requireSecondSigChecked,
     );
     setErrors(validation);
-
+    if (
+      deal.docSpringTemplateId === 'tpl_qbHh2KPqMkXaTyZbr7' &&
+      !investorFormData?.realEstateUseage
+    )
+      return toast.warning('Please select a length option.');
     if (validation.length > 0) return toast.warning('Incomplete form');
     if (!amount) return toast.warning('Please enter a valid investment amount');
 
