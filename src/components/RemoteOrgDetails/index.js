a331a92c7ec571936f3aecbecd6c96f949e19d76
Chase Abbott <cabbott93@gmail.com>
2022-08-18 11:20:32 -0700

    add orgdetails with redirect

--- src/components/RemoteOrgDetails/index.js
+++ src/components/RemoteOrgDetails/index.js
@@ -1,17 +1,21 @@
 import React, { Suspense } from 'react';
-import { useAuth } from '../../auth/useAuth';
+import { useHistory } from 'react-router';
 import Loader from '../utils/Loader';
 
 React.lazy(() => import('passport/PassportDropdown'));
 const OrgDetails = React.lazy(() => import('build/OrgDetails'));
 
 export default function RemoteOrgDetails() {
-  const { userProfile } = useAuth();
+  const history = useHistory();
 
   return (
     <>
       <Suspense fallback={<Loader />}>
-        <OrgDetails user={userProfile} />
+        <OrgDetails
+          redirect={(orgId) => {
+            history.push(`/build?organization_id=${orgId}`);
+          }}
+        />
       </Suspense>
     </>
   );
