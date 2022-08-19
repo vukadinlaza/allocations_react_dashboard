5131c695b960784207c01de2817503df096ebf25
Chase Abbott <cabbott93@gmail.com>
2022-08-18 17:16:25 -0700

    remove comments

--- src/components/RemoteBuildProduct/index.js
+++ src/components/RemoteBuildProduct/index.js
@@ -1,5 +1,4 @@
 import React, { Suspense } from 'react';
-import { useHistory } from 'react-router';
 import { useAuth } from '../../auth/useAuth';
 import Loader from '../utils/Loader';
 
@@ -8,7 +7,6 @@ const BuildProduct = React.lazy(() => import('build/BuildProduct'));
 
 export default function RemoteBuildProduct() {
   const { userProfile } = useAuth();
-  // const history = useHistory();
 
   return (
     <>
@@ -17,7 +15,6 @@ export default function RemoteBuildProduct() {
           user={userProfile}
           redirect={({ organization_slug, deal_id }) => {
             console.log(organization_slug, deal_id);
-            // history.push(`/admin/${organization_slug}/deals/${deal_id}`);
           }}
         />
       </Suspense>
