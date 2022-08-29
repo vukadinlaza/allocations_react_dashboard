952147b6625cc0b1bafc2e0a064ff6487b6cdfdd
Chase Abbott <cabbott93@gmail.com>
2022-08-29 08:51:29 -0700

    update crypto disclaimer

--- src/components/DealOneClick/LandingPage/CryptoDiscPanel/index.js
+++ src/components/DealOneClick/LandingPage/CryptoDiscPanel/index.js
@@ -12,11 +12,11 @@ function CryptoDisclosure() {
         <p>
           The Fund Administrator, Allocations, Inc., is not a Money Service Business and therefore,
           cannot directly process deposits made in the form of Cryptocurrency. Deposits made in
-          Cryptocurrency shall be processed by a third party, Circle Internet Financial Limited
-          (“Circle”) a private limited liability company located in, and organized under the laws of
-          the United States. Circle is a separate legal entity and not affiliated with Allocations,
-          Inc., through common ownership. Virtual currency is not legal tender, and is not backed by
-          the government. Your Crypto Assets are not insured or guaranteed by the FDIC, Securities
+          Cryptocurrency shall be processed by a third party, Coinbase Global, Inc. (“Coinbase”) a
+          publicly traded company located in, and organized under the laws of the United States.
+          Coinbase is a separate legal entity and not affiliated with Allocations, Inc., through
+          common ownership. Virtual currency is not legal tender, and is not backed by the
+          government. Your Crypto Assets are not insured or guaranteed by the FDIC, Securities
           Investor Protection Corporation (SIPC) or any other public or private insurer, including
           against cyber theft or theft by other means. The transfer, purchase and sale of
           cryptocurrency is irrevocable. The nature of cryptocurrency may lead to an increased risk
@@ -29,13 +29,13 @@ function CryptoDisclosure() {
           transaction, if (i) we believe that the transaction relates to any Restricted Activities,
           (ii) we believe the transaction involves money laundering, terrorist financing, fraud or
           any other type of crime or (iii) in response to a subpoena, court order, or other
-          government order. In such instances, regardless of the reason we have blocked, cancelled
-          or reversed your transaction and regardless of the reason we have lifted or reversed that
+          government order. In such instances, regardless of the reason we have blocked, canceled or
+          reversed your transaction and regardless of the reason we have lifted or reversed that
           restriction, we do not guarantee you will be able to reinitiate your transaction at the
           same price or on the same terms as the transaction that we did not process, or that was
-          cancelled or reversed. Legislative and regulatory changes or actions at the state,
-          federal, or international level may adversely affect the use, transfer, exchange and value
-          of virtual currency.
+          canceled or reversed. Legislative and regulatory changes or actions at the state, federal,
+          or international level may adversely affect the use, transfer, exchange and value of
+          virtual currency.
         </p>
       </Box>
     </section>
