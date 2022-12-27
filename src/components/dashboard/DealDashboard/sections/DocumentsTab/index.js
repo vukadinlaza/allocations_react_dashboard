61b470765f31a37b28f95abe19a05e90fb0e6624
chase-abbott <cabbott93@gmail.com>
2022-12-27 13:56:27 -0800

    download all document feature

--- src/components/dashboard/DealDashboard/sections/DocumentsTab/index.js
+++ src/components/dashboard/DealDashboard/sections/DocumentsTab/index.js
@@ -13,9 +13,13 @@ import {
   Typography,
   Tooltip,
   IconButton,
+  Grid,
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import { Button } from '@allocations/design-system';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 import AllocationsTable from '../../../../utils/AllocationsTable';
 import Loader from '../../../../utils/Loader';
 import { titleCase } from '../../../../../utils/helpers';
@@ -266,8 +270,42 @@ const DocumentsTab = ({ classes, data, refetch }) => {
     });
   }
 
+  const handleZip = async (dealName) => {
+    try {
+      const zip = new JSZip();
+
+      if (documentsData.length === 0) {
+        return toast.success('There are no documents to download.');
+      }
+      const files = await Promise.all(
+        documentsData.map((doc) => {
+          const link = doc.docLink.includes('https') ? doc.docLink : `https://${doc.docLink}`;
+          return fetch(link).then((res) => {
+            return res.blob();
+          });
+        }),
+      );
+
+      files.forEach((file, i) =>
+        zip.file(`${documentsData[i].documents.replace('.pdf', '')}.pdf`, file),
+      );
+
+      const content = await zip.generateAsync({ type: 'blob' });
+      saveAs(content, `${dealName}_Investment_Documents.zip`);
+    } catch {
+      toast.error('Error downloading Investment Documents');
+    }
+  };
+
   return (
-    <div className={classes.section}>
+    <Grid container className={classes.section}>
+      <Grid
+        item
+        xs={12}
+        style={{ marginBottom: '15px', display: 'flex', justifyContent: 'flex-end' }}
+      >
+        <Button text="Download Documents" onClick={() => handleZip(data.deal.company_name)} />
+      </Grid>
       <div className={classes.searchContainer}>
         <FormControl variant="outlined" size="small" value={sortField}>
           <InputLabel htmlFor="field-filter">Field</InputLabel>
@@ -299,8 +337,9 @@ const DocumentsTab = ({ classes, data, refetch }) => {
           style={{ margin: '0 1em' }}
         />
       </div>
+
       <AllocationsTable data={documentsData} headers={headers} getCellContent={getCellContent} />
-    </div>
+    </Grid>
   );
 };
 
