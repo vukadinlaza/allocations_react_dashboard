cc7f545295f0f6d6a861f9a7ea8a56fa203e54b0
Adrian Monasterios Gutierrez <adri.suscripciones@gmail.com>
2023-03-03 12:29:14 -0500

    small fix

--- src/components/RetoolPassportUpdate/index.js
+++ src/components/RetoolPassportUpdate/index.js
@@ -25,7 +25,6 @@ const RetoolPassportUpdate = () => {
   if (!data || (currentOrganization && !isUserTheOrgAdmin)) return null;
   return (
     <div style={{ width: '100%', height: '84vh' }}>
-      <div> IPDATE</div>
       <Retool
         url={data.retoolEmbedUrl}
         data={{ user: { email: userProfile.email }, organizationId: currentOrganization }}
